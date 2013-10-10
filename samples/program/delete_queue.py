ec422eec6a7e56ef90fcdf1bacf9c67d4086dd4e
Michel Grenier <michel_grenier@sourceforge.net>
2013-10-10 17:34:53 +0000

    MG sample  of configuration files for dd_subscribe    sample  of program             radar_XFT_messages.py get notifications             delete_queue.py  admin ... delete queue on server

---
+++ samples/program/delete_queue.py
@@ -0,0 +1,42 @@
+# in PIKA an administrative tool to get rid of a Queue
+# on the rabbitmq server
+
+import pika
+import sys
+
+HOST  = sys.argv[1]
+ADMIN = sys.argv[2]
+PW    = sys.argv[3]
+QUEUE = sys.argv[4]
+
+EXCHANGE = sys.argv[5]
+EXCHANGE_TYPE = sys.argv[6]
+
+class DeleteQueueNamed(object):
+
+    def __init__(self, amqp_url):
+        self._connection = None
+        self._channel = None
+        self._url = amqp_url
+        self.run()
+
+    def run(self):
+        self._connection = self.connect()
+        self._connection.ioloop.start()
+
+    def connect(self):
+        return pika.SelectConnection(pika.URLParameters(self._url), self.on_connection_open, stop_ioloop_on_close=True)
+
+    def on_connection_open(self, unused_connection):
+        self._connection.channel(on_open_callback=self.on_channel_open)
+
+    def on_channel_open(self, channel):
+        self._channel = channel
+        self._channel.exchange_declare(self.on_exchange_declareok, EXCHANGE, EXCHANGE_TYPE)
+
+    def on_exchange_declareok(self, unused_frame):
+        self._channel.queue_delete(None, QUEUE, if_unused=False, if_empty=False, nowait=True)
+        self._channel.close()
+        self._connection.close()
+
+DeleteQueueNamed('amqp://%s:%s@%s:5672' % (ADMIN,PW,HOST) + '/%2F' )
---
+++ samples/program/radar_XFT_messages.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+# usage :
+#
+# ./radar_XFT_messages.py
+
+# This code was created using a sample given with python-amqplib
+# It is only checking onto messages for a specific radar
+# It creates an auto-delete queue... so nothing is kept between
+# different sessions
+
+import amqplib.client_0_8 as amqp
+import os,sys,time
+
+print "RADARS are every 10 MINS... "
+print "It may take that time to have something shown"
+
+machine        = "amqp.weather.gc.ca"
+#machine       = "dd.weather.gc.ca"
+#machine       = sys.argv[1]
+user           = "anonymous"
+passwd         = "anonymous"
+
+exchange_realm = "/data"
+exchange_name  = "cmc"
+exchange_type  = "topic"
+
+# getting notifications for the GIF of the XFT radar
+exchange_key   = "exp.dd.notify.radar.PRECIP.GIF.XFT.#"
+
+# connection
+connection = amqp.Connection( machine,userid=user,password=passwd,ssl=False)
+channel    = connection.channel()
+
+# exchange
+channel.access_request(exchange_realm, active=True, read=True)
+channel.exchange_declare(exchange_name, exchange_type, auto_delete=False)
+
+# queue
+_queuename, message_count, consumer_count = channel.queue_declare()
+channel.queue_bind(_queuename, exchange_name, exchange_key)
+
+# amqp callback
+
+def amqp_callback(msg):
+
+    hdr = msg.properties['application_headers']
+    filename = hdr['filename']
+
+    msg.channel.basic_ack(msg.delivery_tag)
+
+    # Cancel this callback
+    if msg.body == 'quit':
+       msg.channel.basic_cancel(msg.consumer_tag)
+
+       self.msg = None
+       self.logger.error('CRITICAL ERROR...')
+       self.logger.error('Requiered to quit the connection')
+       sys.exit(1)
+
+    # send url to user callback
+    user_callback(msg.body)
+
+###################################
+# user callback
+###################################
+
+def user_callback(url):
+    t = time.strftime("%Y%m%d %H:%M:%S",time.gmtime())
+    print("%s %s" % (t,url))
+
+
+# amqp callback activation
+
+channel.basic_consume(_queuename, callback=amqp_callback)
+
+
+# Wait for things to arrive on the queue
+
+while True:
+     try:
+        channel.wait()
+     except:
+        channel.close()
+        connection.close()
+        sys.exit(1)
+
+
+# close connections
+
+channel.close()
+connection.close()
