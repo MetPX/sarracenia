dd95c3f005f8ddb655f10d3b53b024fe1b0ef09a
Khosrow Ebrahimpour <khosrow.ebrahimpour@ssc-spc.gc.ca>
2015-11-20 16:05:57 -0500

    adding vagrant setup

---
+++ vagrant/server/scripts/config-vsftpd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo mkdir /apps
+sudo useradd -b /apps -m 
+echo "px:test" | sudo chpasswd
+sudo mkdir /apps/px/test
+sudo chown px.px /apps/px/test
+
+sudo cp ~vagrant/vsftpd/vsftpd.conf /etc/vsftpd.conf
+sudo chown root.root /etc/vsftpd.conf
+sudo restart vsftpd
--- vagrant/server/scripts/install-deps.sh
+++ vagrant/server/scripts/install-deps.sh
@@ -8,16 +8,17 @@ sudo apt-get install -y python3-amqplib
 sudo apt-get install -y python3-pyinotify
 sudo apt-get install -y python3-psutil
 sudo apt-get install -y python3-pip
+sudo apt-get install -y apache2
+sudo apt-get install -y vsftpd
+sudo apt-get install -y ksh
 
 # python3-paramiko not available for trusty, so we pip install it
 sudo pip3 install paramiko
 
 cd /tmp
-wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.5.6/rabbitmq-server_3.5.6-1_all.deb
+wget -q https://www.rabbitmq.com/releases/rabbitmq-server/v3.5.6/rabbitmq-server_3.5.6-1_all.deb
 sudo dpkg -i rabbitmq-server_3.5.6-1_all.deb 
 
-wget http://iweb.dl.sourceforge.net/project/metpx/sarracenia/metpx-sarracenia_0.1.1_all.deb
+wget -q http://iweb.dl.sourceforge.net/project/metpx/sarracenia/0.1.1/metpx-sarracenia_0.1.1_all.deb
 sudo dpkg -i metpx-sarracenia_0.1.1_all.deb 
 
-# install apache
-sudo apt-get install -y apache2
