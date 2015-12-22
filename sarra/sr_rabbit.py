fa2a29e7fd3e42f20ffbf0b839d35d07e9b0308b
michel grenier <michel.grenier2@canada.ca>
2015-12-22 16:04:40 -0500

    MG strange ... not up to date with mine

--- sarra/sr_rabbit.py
+++ sarra/sr_rabbit.py
@@ -4,7 +4,55 @@ import sys
 import urllib,urllib.parse
 import base64
 import os
+import re
 import socket
+import subprocess
+
+rabbitmqadmin = '.' + os.sep + 'rabbitmqadmin'
+
+#===========================
+# rabbitmqadmin
+#===========================
+
+def exec_rabbitmqadmin(url,options):
+    
+    if not os.path.isfile(rabbitmqadmin):
+       try :
+             import urllib.request, urllib.error
+             urlstr   = "http://"+ url.hostname + ':15672/cli/rabbitmqadmin'
+             req      = urllib.request.Request(urlstr)
+             response = urllib.request.urlopen(req)
+             f = open(rabbitmqadmin,'wb')
+             buf = response.read(10000000)
+             f.write(buf)
+             f.close()
+             os.chmod(rabbitmqadmin,0o755)
+       except :
+             #(stype, svalue, tb) = sys.exc_info()
+             #print("Type: %s, Value: %s" % (stype, svalue))
+             pass
+
+    try :
+           command  = rabbitmqadmin 
+           command += ' -H ' + url.hostname
+           command += ' -u ' + url.username
+           command += ' -p ' + url.password
+           command += ' -f raw_json '
+           command += ' '    + options
+
+           # (status, answer) = subprocess.getstatusoutput(command)
+           return subprocess.getstatusoutput(command)
+    except :
+           #(stype, svalue, tb) = sys.exc_info()
+           #print("Type: %s, Value: %s" % (stype, svalue))
+           pass
+
+    return 0,None
+    
+#===========================
+# direct access to rabbitmq management plugin
+# this is what rabbitmqadmin does under the cover
+#===========================
 
 def rabbitmq_broker_get_exchanges( url, ssl_key_file=None, ssl_cert_file=None ):
         import http.client
@@ -12,7 +60,7 @@ def rabbitmq_broker_get_exchanges( url, ssl_key_file=None, ssl_cert_file=None ):
         path   = "/api/exchanges?columns=name"
 
         if url.scheme == 'amqps':
-            conn = http.client.HTTPSConnection(hostname, "15672", ssl_key_file, ssl_cert_file)
+            conn = http.client.HTTPSConnection(hostname, "15671", ssl_key_file, ssl_cert_file)
         else:
             conn = http.client.HTTPConnection(url.hostname,"15672")
 
@@ -45,3 +93,5 @@ if __name__ == "__main__":
     print(sys.argv[1])
     url = urllib.parse.urlparse(sys.argv[1])
     print(rabbitmq_broker_get_exchanges(url))
+    print(exec_rabbitmqadmin(url,"list exchanges name"))
+
