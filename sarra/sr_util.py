d619943f26b69bc7ea8e02d8e4d7452525bdd2c1
michel grenier <michel.grenier2@canada.ca>
2016-02-04 16:34:23 -0500

    MG cleanup, checksum type are now classes    still need to address in general where to get user and other checksums

--- sarra/sr_util.py
+++ sarra/sr_util.py
@@ -14,7 +14,7 @@
 # Code contributed by:
 #  Michel Grenier - Shared Services Canada
 #  Last Changed   : Sep 22 10:41:32 EDT 2015
-#  Last Revision  : Sep 22 10:41:32 EDT 2015
+#  Last Revision  : Feb  4 09:09:03 EST 2016
 #
 ########################################################################
 #  This program is free software; you can redistribute it and/or modify
@@ -33,99 +33,85 @@
 #
 #
 
-import os,stat,time
+import os,stat,time,sys
 import urllib
 import urllib.parse
 from hashlib import md5
 
 # ===================================
+# checksum_0 class
+# ===================================
+
+class checksum_0(object):
+      def __init__(self):
+          self.value = '0'
+
+      def get_value(self):
+          return self.value
+
+      def update(self,chunk):
+          pass
+
+      def set_path(self,path):
+          pass
+
+# ===================================
+# checksum_d class
+# ===================================
+
+class checksum_d(object):
+      def __init__(self):
+          self.value = '0'
+
+      def get_value(self):
+          self.value = self.filehash.hexdigest()
+          return self.value
+
+      def update(self,chunk):
+          self.filehash.update(chunk)
+
+      def set_path(self,path):
+          self.filehash = md5()
+
+# ===================================
+# checksum_n class
+# ===================================
+
+class checksum_n(object):
+      def __init__(self):
+          self.value = '0'
+
+      def get_value(self):
+          return self.value
+
+      def update(self,chunk):
+          pass
+
+      def set_path(self,path):
+          filename   = os.path.basename(path)
+          self.value = md5(bytes(filename,'utf-8')).hexdigest()
+
+# ===================================
 # Checksum class
 # ===================================
 
-class Checksum:
+class Checksum(object):
       def __init__(self):
-          self.checksum = self.checksum_d
+          self.checksum = checksum_d()
 
-      def from_list(self,flgs):
-          self.checksum = self.checksum_d
+      def from_list(self,iflgs):
+          flgs = iflgs
+
+          if len(iflgs) > 2 and iflgs[:2] == 'z,':
+             flgs = iflgs[2:]
+
+          self.checksum = checksum_d()
           if   flgs == 'd' :
-             self.checksum = self.checksum_d
+             self.checksum = checksum_d()
           elif flgs == 'n' :
-             self.checksum = self.checksum_n
+             self.checksum = checksum_n()
           elif flgs == '0' :
-             self.checksum = self.checksum_0
+             self.checksum = checksum_0()
+          #FIXME ...  where are the sum_scripts?
           else :
              exec(compile(open(flgs).read(), flgs, 'exec'))
-
-      # checksum_0 = checksum for flag 0
-
-      def checksum_0(self, filepath, offset=0, length=0 ):
-          return '0'
-
-      # checksum_d = checksum for flag d
-
-      def checksum_d(self, filepath, offset=0, length=0 ):
-          f = open(filepath,'rb')
-          if offset != 0 : f.seek(offset,0)
-          if length != 0 : data = f.read(length)
-          else:            data = f.read()
-          f.close()
-          return md5(data).hexdigest()
-
-      # checksum_n = checksum for flag n
-
-      def checksum_n( self, filepath, offset=0, length=0 ):
-          filename = os.path.basename(filepath)
-          return md5(bytes(filename,'utf-8')).hexdigest()
-
-# ===================================
-# chunk size from a string value
-# ===================================
-
-def chunksize_from_str(str_value):
-    factor = 1
-    if str_value[-1] in 'bB'   : str_value = str_value[:-1]
-    if str_value[-1] in 'kK'   : factor = 1024
-    if str_value[-1] in 'mM'   : factor = 1024 * 1024
-    if str_value[-1] in 'gG'   : factor = 1024 * 1024 * 1024
-    if str_value[-1] in 'tT'   : factor = 1024 * 1024 * 1024 * 1024
-    if str_value[-1].isalpha() : str_value = str_value[:-1]
-    chunksize = int(str_value) * factor
-
-    return chunksize
-
-class Chunk:
-
-    def __init__(self, chunksize, chksum, filepath ):
-
-        self.chunksize     = chunksize
-        self.chksum        = chksum
-        self.filepath      = filepath
-
-        lstat              = os.stat(filepath)
-        self.fsiz          = lstat[stat.ST_SIZE]
-
-        self.chunksize     = self.fsiz
-        self.block_count   = 1
-        self.remainder     = 0
-
-        if chunksize != 0 and chunksize < self.fsiz :
-           self.chunksize   = chunksize
-           self.block_count = int(self.fsiz / self.chunksize)
-           self.remainder   = self.fsiz % self.chunksize
-           if self.remainder > 0 : self.block_count = self.block_count + 1
-
-    def get_Nblock(self):
-           return self.block_count
-
-    def get(self,current_block):
-
-        if current_block >= self.block_count : return None
-
-        if self.block_count == 1 :
-           data_sum  = self.chksum(self.filepath,0,self.fsiz)
-           return (self.fsiz, 1, 0, 0, data_sum)
-
-        data_sum = self.chksum(self.filepath,current_block*self.chunksize,self.chunksize)
-
-        return (self.chunksize, self.block_count, self.remainder, current_block, data_sum) 
