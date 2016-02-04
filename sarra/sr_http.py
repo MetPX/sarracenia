8704d1de28d73b5b61b7c52e0002dee391ce8400
michel grenier <michel.grenier2@canada.ca>
2016-02-04 16:13:51 -0500

    MG rewrote and onfly_checksum on download

--- sarra/sr_http.py
+++ sarra/sr_http.py
@@ -15,7 +15,7 @@
 #  Michel Grenier - Shared Services Canada
 #  Jun Hu         - Shared Services Canada
 #  Last Changed   : Sep 22 10:41:32 EDT 2015
-#  Last Revision  : Sep 22 10:41:32 EDT 2015
+#  Last Revision  : Feb  4 13:59:22 EST 2016
 #
 ########################################################################
 #  This program is free software; you can redistribute it and/or modify
@@ -36,112 +36,168 @@
 
 import os, urllib.request, urllib.error, sys
 
-def http_download( parent ) :
+class http_transport():
+    def __init__(self) :
+        self.bufsize   = 8192
+        self.chkalgo   = None
+        self.kbytes_ps = 0
 
-    msg          = parent.msg
-    url          = msg.url
-    urlstr       = msg.urlstr
+    def close(self) :
+        pass
 
-    ok, details  = parent.credentials.get(msg.urlcred)
-    if details   : url = details.url 
+    def download( self, parent ):
+        parent.logger.debug("sr_http http_transport download")
+        self.logger  = parent.logger
+        self.parent  = parent
 
-    user         = url.username
-    passwd       = url.password
+        msg          = parent.msg
+        url          = msg.url
+        urlstr       = msg.urlstr
+        token        = msg.url.path[1:].split('/')
+        remote_file  = token[-1]
 
-    try :
-            # create a password manager                
-            if user != None :                          
-                # Add the username and password.
-                password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-                password_mgr.add_password(None, urlstr,user,passwd)
-                handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-                        
-                # create "opener" (OpenerDirector instance)
-                opener = urllib.request.build_opener(handler)
-    
-                # use the opener to fetch a URL
-                opener.open(urlstr)
-    
-                # Install the opener.
-                # Now all calls to urllib2.urlopen use our opener.
-                urllib.request.install_opener(opener)
+        ok, details  = parent.credentials.get(msg.urlcred)
+        if details   : url = details.url 
 
-            # set a byte range to pull from remote file
-            req   = urllib.request.Request(urlstr)
-            str_range = ''
-            if msg.partflg == 'i' :
-               str_range = 'bytes=%d-%d'%(msg.offset,msg.offset+msg.length-1)
-               req.headers['Range'] = str_range
-                   
-            #download file
+        user         = url.username
+        passwd       = url.password
 
-            msg.logger.info('Downloads: %s %s into %s %d-%d' % (urlstr,str_range,msg.local_file,msg.local_offset,msg.length))  
+        if hasattr(self.parent,'kbytes_ps') : self.kbytes_ps = self.parent.kbytes_ps
+        if hasattr(self.parent,'bufsize')   : self.bufsize   = self.parent.bufsize
 
-            response = urllib.request.urlopen(req)
-            #msg.logger.debug('response header = %s' % response.headers)
+        self.chkalgo     = msg.chkalgo
+        self.remote_file = remote_file
 
-            # FIXME  locking for i parts in temporary file ... should stay lock
-            # and file_reassemble... take into account the locking
+        try :
+                # create a password manager                
+                if user != None :                          
+                    # Add the username and password.
+                    password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+                    password_mgr.add_password(None, urlstr,user,passwd)
+                    handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+                            
+                    # create "opener" (OpenerDirector instance)
+                    opener = urllib.request.build_opener(handler)
+        
+                    # use the opener to fetch a URL
+                    opener.open(urlstr)
+        
+                    # Install the opener.
+                    # Now all calls to urllib2.urlopen use our opener.
+                    urllib.request.install_opener(opener)
 
-            if   parent.lock == None or msg.partflg == 'i' :
-                 ok = http_write(response,msg.local_file,msg,parent.bufsize)
+                # set a byte range to pull from remote file
+                req   = urllib.request.Request(urlstr)
+                str_range = ''
+                if msg.partflg == 'i' :
+                   str_range = 'bytes=%d-%d'%(msg.offset,msg.offset+msg.length-1)
+                   req.headers['Range'] = str_range
+                       
+                #download file
 
-            elif parent.lock == '.' :
-                 local_lock = ''
-                 local_dir  = os.path.dirname (msg.local_file)
-                 if local_dir != '' : local_lock = local_dir + os.sep
-                 local_lock += '.' + os.path.basename(msg.local_file)
-                 ok = http_write(response,local_lock,msg,parent.bufsize)
-                 if os.path.isfile(msg.local_file) : os.remove(msg.local_file)
-                 os.rename(local_lock, msg.local_file)
-            
-            elif parent.lock[0] == '.' :
-                 local_lock  = msg.local_file + parent.lock
-                 ok = http_write(response,local_lock,msg,parent.bufsize)
-                 if os.path.isfile(msg.local_file) : os.remove(msg.local_file)
-                 os.rename(local_lock, msg.local_file)
+                msg.logger.info('Downloads: %s %s into %s %d-%d' % (urlstr,str_range,msg.local_file,msg.local_offset,msg.length))  
 
-            msg.log_publish(201,'Downloaded')
+                response = urllib.request.urlopen(req)
+                #msg.logger.debug('response header = %s' % response.headers)
 
-            return ok
+                # FIXME  locking for i parts in temporary file ... should stay lock
+                # and file_reassemble... take into account the locking
 
-    except urllib.error.HTTPError as e:
-           msg.logger.error('Download failed %s ' % urlstr)
-           msg.logger.error('Server couldn\'t fulfill the request. Error code: %s, %s', e.code, e.reason)
-    except urllib.error.URLError as e:
-           msg.logger.error('Download failed %s ' % urlstr)
-           msg.logger.error('Failed to reach server. Reason: %s', e.reason)
-    except:
-           (stype, svalue, tb) = sys.exc_info()
-           msg.logger.error('Download failed %s ' % urlstr)
-           msg.logger.error('Unexpected error Type: %s, Value: %s' % (stype, svalue))
+                if   parent.lock == None or msg.partflg == 'i' :
+                     ok = self.http_write(response,msg.local_file,msg)
 
-    msg.log_publish(499,'http download problem')
-    msg.logger.error("Could not download")
+                elif parent.lock == '.' :
+                     local_lock = ''
+                     local_dir  = os.path.dirname (msg.local_file)
+                     if local_dir != '' : local_lock = local_dir + os.sep
+                     local_lock += '.' + os.path.basename(msg.local_file)
+                     ok = self.http_write(response,local_lock,msg)
+                     if os.path.isfile(msg.local_file) : os.remove(msg.local_file)
+                     os.rename(local_lock, msg.local_file)
+                
+                elif parent.lock[0] == '.' :
+                     local_lock  = msg.local_file + parent.lock
+                     ok = self.http_write(response,local_lock,msg)
+                     if os.path.isfile(msg.local_file) : os.remove(msg.local_file)
+                     os.rename(local_lock, msg.local_file)
 
-    return False
+                msg.onfly_checksum = self.checksum
 
-def http_write(req,local_file,msg,bufsize) :
-    if not os.path.isfile(local_file) :
-       fp = open(local_file,'w')
-       fp.close
+                msg.log_publish(201,'Downloaded')
 
-    fp = open(local_file,'r+b')
-    if msg.local_offset != 0 : fp.seek(msg.local_offset,0)
+                return ok
 
-    # should not worry about length...
-    # http provides exact data
+        except urllib.error.HTTPError as e:
+               msg.logger.error('Download failed %s ' % urlstr)
+               msg.logger.error('Server couldn\'t fulfill the request. Error code: %s, %s', e.code, e.reason)
+        except urllib.error.URLError as e:
+               msg.logger.error('Download failed %s ' % urlstr)
+               msg.logger.error('Failed to reach server. Reason: %s', e.reason)
+        except:
+               (stype, svalue, tb) = sys.exc_info()
+               msg.logger.error('Download failed %s ' % urlstr)
+               msg.logger.error('Unexpected error Type: %s, Value: %s' % (stype, svalue))
 
-    while True:
-          chunk = req.read(bufsize)
-          if not chunk : break
-          fp.write(chunk)
+        msg.log_publish(499,'http download problem')
+        msg.logger.error("Could not download")
 
-    fp.close()
+        return False
 
-    msg.log_publish(201,'Downloaded')
+    def http_write(self,req,local_file,msg) :
 
-    return True
+        # on fly checksum 
+
+        chk           = self.chkalgo
+        self.checksum = None
+
+        # trottle 
+
+        cb = None
+
+        if self.kbytes_ps > 0.0 :
+           cb = self.trottle
+           d1,d2,d3,d4,now = os.times()
+           self.tbytes     = 0.0
+           self.tbegin     = now + 0.0
+           self.bytes_ps   = self.kbytes_ps * 1024.0
+
+        if not os.path.isfile(local_file) :
+           fp = open(local_file,'w')
+           fp.close
+
+        fp = open(local_file,'r+b')
+        if msg.local_offset != 0 : fp.seek(msg.local_offset,0)
+
+
+        if chk : chk.set_path(self.remote_file)
+
+        # should not worry about length...
+        # http provides exact data
+
+        while True:
+              chunk = req.read(self.bufsize)
+              if not chunk : break
+              fp.write(chunk)
+              if chk : chk.update(chunk)
+              if cb  : cb(chunk)
+
+        fp.close()
+
+        if chk : self.checksum = chk.get_value()
+
+        msg.log_publish(201,'Downloaded')
+
+        return True
+
+    # trottle
+    def trottle(self,buf) :
+        self.logger.debug("http_transport trottle")
+        self.tbytes = self.tbytes + len(buf)
+        span = self.tbytes / self.bytes_ps
+        d1,d2,d3,d4,now = os.times()
+        rspan = now - self.tbegin
+        if span > rspan :
+           time.sleep(span-rspan)
 
 # ===================================
 # self_test
@@ -149,10 +205,14 @@ def http_write(req,local_file,msg,bufsize) :
 
 try    :
          from sr_config         import *
+         from sr_consumer       import *
          from sr_message        import *
+         from sr_util           import *
 except : 
          from sarra.sr_config   import *
+         from sarra.sr_consumer import *
          from sarra.sr_message  import *
+         from sarra.sr_util     import *
 
 class test_logger:
       def silence(self,str):
@@ -167,41 +227,64 @@ def self_test():
 
     logger = test_logger()
 
+    chkclass = Checksum()
+    chkclass.from_list('d')
+    chkalgo = chkclass.checksum
+
+
     # config setup
     cfg = sr_config()
     cfg.defaults()
     cfg.general()
     cfg.debug  = True
     cfg.logger = logger
+    cfg.kbytes_ps = 10.0
+
+    #setup consumer to catch first post
+    cfg.broker         = urllib.parse.urlparse("amqp://anonymous:anonymous@ddi.cmc.ec.gc.ca/")
+    cfg.bindings       = [ ( 'xpublic', 'v02.post.#') ]
+    cfg.user_cache_dir = os.getcwd()
+    cfg.config_name    = "test"
+    cfg.queue_name     = None
+
+    consumer = sr_consumer(cfg)
+
+    i = 0
+    while True :
+          ok, msg = consumer.consume()
+          if ok: break
+
+    cfg.msg = msg
+    cfg.msg.chkalgo = None
+    cfg.msg.local_file = "toto"
+    cfg.msg.local_offset = 0
 
     try:
-           cfg.msg         = sr_message(cfg.logger)
-           cfg.msg.start_timer()
-           cfg.msg.topic   = 'v02.post.test'
-           cfg.msg.host    = 'localhost'
-           cfg.msg.user    = 'localuser'
-           cfg.msg.notice  = 'noticest.test'
+             
+          tr = http_transport()
 
-           cfg.msg.urlcred = "http://dd1.wxod-stage.cmc.ec.gc.ca"
-           cfg.msg.urlstr  = "http://dd1.wxod-stage.cmc.ec.gc.ca/citypage_weather/xml/ON/s0000623_e.xml"
-           cfg.msg.url     = urllib.parse.urlparse(cfg.msg.urlstr)
-           cfg.msg.partflg = '1'
-           cfg.msg.local_file   = './toto'
-           cfg.msg.local_offset = 0
-           cfg.msg.length       = 100000
-                   
-           cfg.lock = None
-           http_download(cfg)
-           cfg.lock = '.'
-           http_download(cfg)
-           cfg.lock = '.tmp'
-           http_download(cfg)
+          cfg.lock = None
+          tr.download(cfg)
+          logger.debug("checksum = %s" % cfg.msg.onfly_checksum)
+          cfg.lock = '.'
+          tr.download(cfg)
+          logger.debug("checksum = %s" % cfg.msg.onfly_checksum)
+          cfg.lock = '.tmp'
+          tr.download(cfg)
+          logger.debug("checksum = %s" % cfg.msg.onfly_checksum)
+          cfg.msg.chkalgo = chkalgo
+          tr.download(cfg)
+          logger.debug("checksum = %s" % cfg.msg.onfly_checksum)
+          logger.debug("checksum = %s" % cfg.msg.checksum)
 
     except:
-           logger.error("sr_http TEST FAILED")
-           (stype, svalue, tb) = sys.exc_info()
-           logger.error('Unexpected error Type: %s, Value: %s' % (stype, svalue))
-           sys.exit(2)
+          logger.error("sr_http TEST FAILED")
+          (stype, svalue, tb) = sys.exc_info()
+          logger.error('Unexpected error Type: %s, Value: %s' % (stype, svalue))
+          sys.exit(2)
+
+    os.unlink(consumer.queuepath)
+    consumer.close()
 
     print("sr_http TEST PASSED")
     sys.exit(0)
