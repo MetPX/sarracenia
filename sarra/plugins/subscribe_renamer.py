a2924e57ee6ac1694e4ffde392ed2c80d135d73a
michel grenier <michel.grenier2@canada.ca>
2016-03-01 12:44:39 -0500

    MG subscribe renamer (from dms-op3 setup)

---
+++ sarra/plugins/subscribe_renamer.py
@@ -0,0 +1,33 @@
+import sys, os, os.path, time, stat
+
+# this renamer
+# takes px name     : CACN00_CWAO_081900__WZS_14623:pxatx:CWAO:CA:5:Direct:20081008190602
+# add datetimestamp : CACN00_CWAO_081900__WZS_14623:pxatx:CWAO:CA:5:Direct:20081008190602:20081008190612
+
+class Renamer(object):
+
+      def __init__(self) :
+          pass
+
+      # old in metpx
+      #def perform(self,path):
+          #datestr = time.strftime('%Y%m%d%H%M%S',time.localtime())
+          #new_path = path + ':' + datestr
+          #return new_path
+
+      def perform(self,parent):
+          import time
+
+          datestr = time.strftime(':%Y%m%d%H%M%S',time.localtime())
+
+          parent.msg.local_file        += datestr
+          parent.msg.headers['rename'] += datestr
+
+          return True
+
+renamer=Renamer()
+self.on_message=renamer.perform
+
+# test interactif
+#print renamer.perform(sys.argv[1])
+
