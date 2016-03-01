4e0dd2a8e3c38b1adfabc419ba11848693e8ab5e
michel grenier <michel.grenier2@canada.ca>
2016-03-01 12:47:25 -0500

    MG added comments to explain how and what it does

--- sarra/plugins/subscribe_renamer.py
+++ sarra/plugins/subscribe_renamer.py
@@ -1,6 +1,9 @@
 import sys, os, os.path, time, stat
 
-# this renamer
+# This is an  example of the usage of on_message script
+# to rename the product when it is downloaded
+# It adds a ':datetime' to the name of the product
+#
 # takes px name     : CACN00_CWAO_081900__WZS_14623:pxatx:CWAO:CA:5:Direct:20081008190602
 # add datetimestamp : CACN00_CWAO_081900__WZS_14623:pxatx:CWAO:CA:5:Direct:20081008190602:20081008190612
 
