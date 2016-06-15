d26e0652bd2b141f51f9bd2fea7e988702fe860d
Peter Silva <petersilva@sourceforge.net>
2016-06-15 06:22:51 -0400

    word smithery in source guide. removing overwrites of config files from test_sr_watch.sh

--- test/test_sr_watch.sh
+++ test/test_sr_watch.sh
@@ -1,29 +1,29 @@
-#!/bin/ksh
+#!/bin/bash
 
 # This test suppose rabbitmq server installed
 # with default configuration  guest,guest administrator
 
 # getting rabbitmqadmin
 
-wget http://localhost:15672/cli/rabbitmqadmin
-chmod 755 rabbitmqadmin
+#wget http://localhost:15672/cli/rabbitmqadmin
+#chmod 755 rabbitmqadmin
 
 # configuring tester user as sarra requieres
 
-./rabbitmqadmin -u guest -p guest declare user \
-     name=tester password=testerpw tags=
+#./rabbitmqadmin -u guest -p guest declare user \
+#     name=tester password=testerpw tags=
 
-./rabbitmqadmin -u guest -p guest declare permission \
-     vhost=/  user=tester \
-     configure='^q_tester.*$' write='xs_tester' read='^q_tester.*$|^xl_tester$'
+#./rabbitmqadmin -u guest -p guest declare permission \
+#     vhost=/  user=tester \
+#     configure='^q_tester.*$' write='xs_tester' read='^q_tester.*$|^xl_tester$'
 
-./rabbitmqadmin -u guest -p guest declare exchange \
-     name=xs_tester type=topic auto_delete=false durable=true
+#./rabbitmqadmin -u guest -p guest declare exchange \
+#     name=xs_tester type=topic auto_delete=false durable=true
 
-./rabbitmqadmin -u guest -p guest declare exchange \
-     name=xs_guest type=topic auto_delete=false durable=true
+#./rabbitmqadmin -u guest -p guest declare exchange \
+#    name=xs_guest type=topic auto_delete=false durable=true
 
-rm ~/.config/sarra/credentials.conf 2>/dev/null
+#rm ~/.config/sarra/credentials.conf 2>/dev/null
 
 cat << EOF > toto
 0 123456789abcde
@@ -59,7 +59,7 @@ echo ========================================
 echo sr_watch -u file:${PWD}/ -e IN_CLOSE_WRITE -to alta
 echo touch ./toto
 
-../sarra/sr_watch.py -u file:${PWD}/ -e IN_CLOSE_WRITE -to alta &
+../sarra/sr_watch.py -u file:${PWD}/ -e modified -to alta &
 PID=$!
 sleep 2
 touch ./toto
@@ -82,18 +82,18 @@ kill -9 $PID
 echo
 
 
-mkdir -p ~/.config/sarra 2> /dev/null
-cat << EOF > ~/.config/sarra/credentials.conf
-amqp://tester:testerpw@localhost
-EOF
+#mkdir -p ~/.config/sarra 2> /dev/null
+#cat << EOF > ~/.config/sarra/credentials.conf
+#amqp://tester:testerpw@localhost
+#EOF
 
 echo ========================================
 echo credential file
-echo sr_watch -u file:${PWD}/ -e IN_DELETE -b amqp://localhost -to alta
+echo sr_watch -u file:${PWD}/ -e deleted -b amqp://localhost -to alta
 echo rm ./toto
 
 mv toto2 toto
-../sarra/sr_watch.py -u file:${PWD}/ -e IN_DELETE -b amqp://localhost -to alta &
+../sarra/sr_watch.py -u file:${PWD}/ -e deleted -b amqp://localhost -to alta &
 PID=$!
 sleep 2
 rm ./toto
