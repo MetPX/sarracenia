1ee193e5d3a7590b0c086c57f5c8ae0705779842
petersilva <petersilva@sourceforge.net>
2016-09-30 08:41:24 -0400

    obsolete paths removed.

--- test/flow_cleanup.sh
+++ test/flow_cleanup.sh
@@ -22,9 +22,6 @@ adminpw="`awk ' /bunnymaster:.*\@localhost/ { sub(/^.*:/,""); sub(/\@.*$/,""); p
 queues_to_delete="`rabbitmqadmin -H localhost -u bunnymaster -p ${adminpw} -f tsv list queues | awk ' ( NR > 1 ) { print $1; }; '`"
 
 
-rm ~/.config/sarra/sarra/t_prime_dd.conf
-rm ~/.config/sarra/subscribe/t_sub.conf
-
 for q in $queues_to_delete; do
     rabbitmqadmin -H localhost -u bunnymaster -p "${adminpw}" delete queue name=$q
 done
