1d8d5a680b726aebf6c0b6b6b7999e29bfeb6e06
Ian Cameron <cameroni830@gmail.com>
2016-11-24 16:26:18 +0000

    Included more error checking (checking if file is sent, difference between original and copied, and if plugin created unique file)

--- test/test_sr_sender.sh
+++ test/test_sr_sender.sh
@@ -31,6 +31,7 @@ sender_file=sender_file.txt
 #Files created by plugin scripts
 on_msg_file="on_msg_file.txt"
 do_send_file="do_send_file.txt"
+on_post_file="on_post_file.txt"
 
 cat << EOF > $file_origin/$sender_file
 0 123456789abcde
@@ -50,8 +51,8 @@ cp ./sender/test5.conf $file_origin/sender_test5.conf
 
 chmod 777 $file_origin/$sender_file
 
-#Checks if the file exists in final destination using sender, PASSED if exists, FAILED if not 
-function check_destination {
+#Checks if the file exists in final destination using sr_sender, PASSED if exists, FAILED if not 
+function check_sender {
 
 	#TODO check contents of file using diff
         initial_dest=$file_origin/$sender_file
@@ -81,7 +82,7 @@ function test_post_doc {
 		-to test_cluster > /dev/null 2>&1		\
 		/
 	sleep 3
-	check_destination "1"
+	check_sender "1"
 	RET=$?
 	sr_sender $file_origin/sender_test1.conf stop > /dev/null 2>&1
 	return $RET
@@ -103,7 +104,7 @@ function test_parts {
 		--parts i,32B					\
 		/
         sleep 3
-	check_destination "2"
+	check_sender "2"
 	RET=$?	
         sr_sender $file_origin/sender_test2.conf stop > /dev/null 2>&1
 	return $RET
@@ -122,7 +123,8 @@ function test_plugin_msg {
 	sr_post -b amqp://$exchange:$credentials@$host/ 	\
 		-u sftp://$sender@$host/ 			\
 		-p $file_origin/$sender_file	 		\
-		-to test_cluster  > /dev/null 2>&1		\
+		-to test_cluster  				\
+		--flow "$file_destination" > /dev/null 2>&1	\
 		/
 	sleep 3
 
@@ -132,9 +134,15 @@ function test_plugin_msg {
 	result_msg="PASSED"
 	RET=0
 
-        diff $initial_dest $final_dest >/dev/null 2>&1
-        if [ $? -eq 1 ] || [ ! -f $final_dest ]; then
-                result_msg="FAILED... sr_sender"
+	if [ -f $final_dest ]; then
+        	diff $initial_dest $final_dest >/dev/null 2>&1
+	else
+		result_msg="FAILED... file not sent"
+		RET=1
+	fi
+		
+	if [ $? -eq 1 ]; then
+                result_msg="FAILED... difference in content"
                 rm $final_dest
                 RET=1
         elif [ ! -f $script_file ]; then
@@ -144,9 +152,6 @@ function test_plugin_msg {
 		rm $final_dest
         fi
 
-#	check_destination "${1}"
-#	RET=$?
-
 	sr_sender $file_origin/sender_test3.conf stop > /dev/null 2>&1
 	echo $result_msg
 	return $RET
@@ -154,26 +159,42 @@ function test_plugin_msg {
 
 function test_plugin_send {
 
-	echo "Testing do_send plugin"
 	#Using do_send script
 	sr_sender --reset $file_origin/sender_test4.conf start > /dev/null 2>&1
 	sleep 3
 	sr_post -b amqp://$exchange:$credentials@$host/ 	\
 		-u sftp://$sender@$host/ 			\
-		-p $file_origin/$sender_file 		\
+		-p $file_origin/$sender_file 			\
 		-to test_cluster > /dev/null 			\
-		--flow "$file_destination" > /dev/null 2>&1			\
+		--flow "$file_destination" > /dev/null 2>&1	\
 		/
 	sleep 3
-	check_destination "4"
-	RET=$?
+
+	initial_dest=$file_origin/$sender_file
+        final_dest=$file_destination/$sender_file
+        result_msg="PASSED"
+        RET=0
+
+        if [ ! -f $final_dest ]; then
+		result_msg="FAILED... file not sent"
+                RET=1
+        else
+		diff $initial_dest $final_dest >/dev/null 2>&1
+        fi
+
+        if [ $? -eq 1 ]; then
+                result_msg="FAILED... difference in content"
+                rm $final_dest
+                RET=1
+        fi
+
 	sr_sender $file_origin/sender_test4.conf stop > /dev/null 2>&1
+	echo $result_msg
 	return $RET
 }
 
 function test_plugin_post {
 
-	echo "Testing on_post plugin"
 	#Using on_post script
 	sr_sender --reset $file_origin/sender_test5.conf start > /dev/null 2>&1
 	sleep 3
@@ -184,14 +205,38 @@ function test_plugin_post {
 		--flow "$file_destination"     			\
 		/
 	sleep 3
-	check_destination "5"
-	RET=$?
+
+	initial_dest=$file_origin/$sender_file
+        final_dest=$file_destination/$sender_file
+        script_file=$file_destination/$on_post_file
+        result_msg="PASSED"
+        RET=0
+
+        if [ -f $final_dest ]; then
+                diff $initial_dest $final_dest >/dev/null 2>&1
+        else
+                result_msg="FAILED... file not sent"
+                RET=1
+        fi
+
+        if [ $? -eq 1 ]; then
+                result_msg="FAILED... difference in content"
+                rm $final_dest
+                RET=1
+        elif [ ! -f $script_file ]; then
+                result_msg="FAILED... on_post plugin"
+                RET=1
+        else
+                rm $final_dest
+        fi
+
 	sr_sender $file_origin/sender_test5.conf stop > /dev/null 2>&1
+	echo $result_msg
 	return $RET
 }
 
 RESULT=0
-# Run tests
+#Run tests
 echo "Sending file by using post_document_root instead of directory..."
 test_post_doc
 if [ $? -eq 1 ]; then
@@ -207,16 +252,16 @@ test_plugin_msg
 if [ $? -eq 1 ]; then
         RESULT=1
 fi
-#echo "Testing do_send plugin script..."
-#test_plugins_send
-#if [ $? -eq 1 ]; then
-#        RESULT=1
-#fi
-#echo "Testing on_post plugin script..."
-#test_plugins_post
-#if [ $? -eq 1 ]; then
-#        RESULT=1
-#fi
+echo "Testing do_send plugin script..."
+test_plugin_send
+if [ $? -eq 1 ]; then
+        RESULT=1
+fi
+echo "Testing on_post plugin script..."
+test_plugin_post
+if [ $? -eq 1 ]; then
+        RESULT=1
+fi
 
 rm $file_origin/$sender_file
 rm -r $file_origin
