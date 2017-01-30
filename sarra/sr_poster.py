9c2382ec43b64afc786a78d942e5b5b0f3ecf1f9
petersilva <petersilva@sourceforge.net>
2017-01-29 22:49:40 -0500

    one blank line...

--- sarra/sr_poster.py
+++ sarra/sr_poster.py
@@ -15,6 +15,7 @@
 #  first shot     : Dec 23 12:30:53 EST 2015
 #
 ########################################################################
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
