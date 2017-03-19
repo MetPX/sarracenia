93f6b71f141c7af44c0490b7ba2f10b20f632107
petersilva <petersilva@sourceforge.net>
2017-03-19 09:25:02 -0400

    Turned sr_log2save.py into a filter.

--- tools/sr_log2save.py
+++ tools/sr_log2save.py
@@ -2,6 +2,9 @@
 
 help_str =  \
 """
+ 
+   FIXME: early days, do not know if this works yet!
+
    sr_convert_log2save <logfile>
 
    sr_log2save reads the _log lines from the given file, assumed to be a log of a sarracenia component, where
@@ -11,32 +14,30 @@ help_str =  \
    by sr_shovel and sr_sender for -restore, and -restore2queue modes.
 
 """
-import json
 import sys
-import urllib
-import urllib.parse
 
 if len(sys.argv) < 2 :
-   print( help_str )
+   log_file = sys.stdin
+else:
+   log_file = open( sys.argv[1], "r" ) 
 
-
-with open( sys.argv[1], "r" ) as log_file:
-    for m in log_file:
+for m in log_file:
         f = m.split()
 
         if f[3][-4:]  != '_log' :
            continue
 
-        #print( "line:  %s" % m )
         timestamp=f[4].split('=')[1]
         
         theslash=10+f[5][10:].index('/')
-        
         path=f[5][theslash+1:]
         newurl=f[5][0:theslash]
+
         notice="%s %s %s" % ( timestamp, newurl, path )
 
-        jsonstr=' '.join(f[6:])[8:]
+        headers=' '.join(f[6:])[8:]
         topic='v02.post' + path.replace('/','.')
 
-        print ( "[ \"%s\", %s, \"%s\" ] " % ( topic, jsonstr, notice ))
+        print ( "[ \"%s\", %s, \"%s\" ] " % ( topic, headers, notice ))
+
+log_file.close()
