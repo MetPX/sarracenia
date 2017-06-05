c97b2c5f10150f5ca95d179239a8a18da7ada824
Peter Silva <petersilva@sourceforge.net>
2017-06-05 17:02:00 -0400

    get rid of rr_* shovel config deletion... there is strange behaviour for self-test.

--- test/flow_cleanup.sh
+++ test/flow_cleanup.sh
@@ -34,10 +34,9 @@ for cf in ${templates}; do
     rm $HOME/.config/sarra/${cf}
 done
 
-for cf in $HOME/.config/sarra/shovel/rr*.conf  ; do
-    rm ${cf}
-done
-
+#for cf in $HOME/.config/sarra/shovel/rr*.conf  ; do
+#    rm ${cf}
+#done
 
 
 if [ -f .httpdocroot ]; then
