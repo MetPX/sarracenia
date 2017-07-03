1b5bfa8beccae4bfe835f993370d6195949cdbc8
petersilva <petersilva@sourceforge.net>
2017-07-03 10:58:46 -0400

    normal test.sh.

--- c/test.sh
+++ c/test.sh
@@ -1,8 +1,7 @@
- touch haha
 export SR_POST_CONFIG="`pwd`/post.conf"
 export LD_PRELOAD=`pwd`/libsrshim.so.1.0.0
 
-#cp sr_poc.c ~/test/hoho_my_darling.txt
-#ln -s hoho `pwd`/haha
+cp sr_poc.c ~/test/hoho_my_darling.txt
+ln -s hoho `pwd`/haha
 rm `pwd`/haha
 
