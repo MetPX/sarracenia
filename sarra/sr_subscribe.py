9626d0e7ab6c45074ad4cd38e16a752f09f1b2d1
petersilva <petersilva@sourceforge.net>
2017-09-04 11:39:35 -0400

    replaced 'corrected header'  warning message with 'missing header default set' debug message.

--- sarra/sr_subscribe.py
+++ sarra/sr_subscribe.py
@@ -279,26 +279,26 @@ class sr_subscribe(sr_instances):
 
     def __on_message__(self):
 
-        # correct a message without a source
+        # apply default to a message without a source
         ex = self.msg.exchange
         if not 'source' in self.msg.headers :
            if len(ex) > 3 and ex[:3] == 'xs_' : self.msg.headers['source'] = ex[3:].split('_')[0]
            elif self.source                   : self.msg.headers['source'] = self.source
            else                               : self.msg.headers['source'] = self.broker.username
-           self.logger.warning("corrected message with headers['source'] = %s" % self.msg.headers['source'])
+           self.logger.debug("message missing header, set default headers['source'] = %s" % self.msg.headers['source'])
 
-        # correct a message without an origin cluster
+        # apply default to a message without an origin cluster
         if not 'from_cluster' in self.msg.headers :
            if self.cluster : self.msg.headers['from_cluster'] = self.cluster
            else            : self.msg.headers['from_cluster'] = self.broker.netloc.split('@')[-1] 
-           self.logger.warning("corrected message with headers['from_cluster'] = %s" % self.msg.headers['from_cluster'])
+           self.logger.debug("message missing header, set default headers['from_cluster'] = %s" % self.msg.headers['from_cluster'])
 
-        # correct a message without routing clusters
+        # apply default to a message without routing clusters
         if not 'to_clusters' in self.msg.headers :
            if self.to_clusters  : self.msg.headers['to_clusters'] = self.to_clusters
            elif self.post_broker: self.msg.headers['to_clusters'] = self.post_broker.netloc.split('@')[-1] 
            if 'to_clusters' in self.msg.headers :
-              self.logger.warning("corrected message with headers['to_clusters'] = %s" % self.msg.headers['to_clusters'])
+              self.logger.debug("message missing header, set default headers['to_clusters'] = %s" % self.msg.headers['to_clusters'])
            else:
               self.logger.warning("message without headers['to_clusters']")
 
