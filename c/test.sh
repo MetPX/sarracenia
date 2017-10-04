f063082bffa7b696e88bb7e7d59d144a998f79f5
Peter Silva <petersilva@sourceforge.net>
2017-10-04 18:48:06 -0400

    C: shim testing adding debug support.

--- c/test.sh
+++ c/test.sh
@@ -1,10 +1,12 @@
 export SR_POST_CONFIG=`pwd`/test_post.conf
 export LD_PRELOAD=`pwd`/libsrshim.so.1.0.0
 
-echo "ldd /usr/bin/python"
-ldd /usr/bin/python2.7
+#echo "ldd /usr/bin/python"
+#ldd /usr/bin/python2.7
 
 #export LD_DEBUG=bindings
+
+export SRSHIMDEBUG=lala
 set -x
 /usr/bin/python2.7 pyiotest
 cp libsrshim.c ~/test/hoho_my_darling.txt
