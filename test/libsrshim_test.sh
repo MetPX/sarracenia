fb52cf2e47f4ff34efe0741076d5738479c37bae
michel grenier <michel.grenier8@canada.ca>
2017-10-17 21:32:46 +0000

    MG tools to verify libsrshim

---
+++ test/libsrshim_test.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+tstdir="`pwd`"
+httpdocroot=`cat $tstdir/.httpdocroot`
+cd
+
+function application_dirs {
+python3 << EOF
+import appdirs
+
+cachedir  = appdirs.user_cache_dir('sarra','science.gc.ca')
+cachedir  = cachedir.replace(' ','\ ')
+print('export CACHEDIR=%s'% cachedir)
+
+confdir = appdirs.user_config_dir('sarra','science.gc.ca')
+confdir = confdir.replace(' ','\ ')
+print('export CONFDIR=%s'% confdir)
+
+logdir  = appdirs.user_log_dir('sarra','science.gc.ca')
+logdir  = logdir.replace(' ','\ ')
+print('export LOGDIR=%s'% logdir)
+
+EOF
+}
+
+eval `application_dirs`
+
+# function to see if same amount of files
+
+function wait_dir_to_be_the_same {
+
+       COUNT=`find "$httpdocroot"/cfr -type f -print | wc -l`
+       COUNT2=`find "$httpdocroot"/cfr -type l -print | wc -l`
+       TCOUNT=$(($COUNT+$COUNT2))
+       sleep 5
+       MCOUNT=`find "$httpdocroot"/cfile -type f -print | wc -l`
+       MCOUNT2=`find "$httpdocroot"/cfile -type l -print | wc -l`
+       TMCOUNT=$(($MCOUNT+$MCOUNT2))
+       while [ "${TMCOUNT}" != "${TCOUNT}" ]; do
+             sleep 5
+             MCOUNT=`find "$httpdocroot"/cfile -type f -print | wc -l`
+             MCOUNT2=`find "$httpdocroot"/cfile -type l -print | wc -l`
+             TMCOUNT=$(($MCOUNT+$MCOUNT2))
+             echo "(${TMCOUNT} expecting ${TCOUNT})"
+       done
+}
+
+# sr_subscribe initial start (no sr_cpost)
+
+sr_cpost stop veille_f34 > /dev/null 2>&1
+rm "$LOGDIR"/sr_cpost_veille_f34*.log > /dev/null
+
+sr_subscribe stop cfile_f44 > /dev/null 2>&1
+rm "$LOGDIR"/sr_subscribe_cfile_f44*.log > /dev/null
+
+# preventive cleanup (previous runs)
+
+find "$httpdocroot"/cfr -type f -print | grep COPY | xargs -n1 rm 2> /dev/null
+find "$httpdocroot"/cfr -type l -print | grep LINK | xargs -n1 rm 2> /dev/null
+find "$httpdocroot"/cfr -type f -print | grep LINK | xargs -n1 rm 2> /dev/null
+find "$httpdocroot"/cfr -type f -print | grep MOVE | xargs -n1 rm 2> /dev/null
+
+find "$httpdocroot"/cfile -type f -print | grep COPY | xargs -n1 rm 2> /dev/null
+find "$httpdocroot"/cfile -type l -print | grep LINK | xargs -n1 rm 2> /dev/null
+find "$httpdocroot"/cfile -type f -print | grep LINK | xargs -n1 rm 2> /dev/null
+find "$httpdocroot"/cfile -type f -print | grep MOVE | xargs -n1 rm 2> /dev/null
+
+sr_subscribe start cfile_f44 > /dev/null 2>&1
+
+# setting up libsrshim
+
+export SR_POST_CONFIG="${CONFDIR}/cpost/veille_f34.conf"
+export LD_PRELOAD=${tstdir}/../c/libsrshim.so.1.0.0
+rm /tmp/libsrshim.log.tmp 2> /dev/null
+
+# copy 
+
+echo "checking sr_cpost copy"
+find "$httpdocroot"/cfr -type f -print                | xargs -iAAA cp AAA AAA.COPY  >> /tmp/libsrshim.log.tmp 2>&1
+find "$httpdocroot"/cfr -type f -print | grep -v COPY | xargs -iAAA cp AAA AAA.COPY2 >> /tmp/libsrshim.log.tmp 2>&1
+wait_dir_to_be_the_same copy
+echo "success"
+
+# move 
+
+echo "checking sr_cpost move"
+find "$httpdocroot"/cfr -type f -print | grep -v COPY | xargs -iAAA  mv AAA.COPY2 AAA.MOVE  >> /tmp/libsrshim.log.tmp 2>&1
+wait_dir_to_be_the_same move
+echo "success"
+
+# softlink 
+
+echo "checking sr_cpost softlink"
+find "$httpdocroot"/cfr -type f -print | grep -v COPY | grep -v MOVE | xargs -iAAA  ln -s AAA AAA.SLINK >> /tmp/libsrshim.log.tmp 2>&1
+wait_dir_to_be_the_same softlink
+echo "success"
+
+# hardlink 
+
+echo "checking sr_cpost hardlink"
+find "$httpdocroot"/cfr -type f -print | grep -v COPY | grep -v MOVE | grep -v LINK | xargs -iAAA ln AAA AAA.HLINK >> /tmp/libsrshim.log.tmp 2>&1
+wait_dir_to_be_the_same hardlink
+echo "success"
+
+# hardlink 
+
+echo "checking sr_cpost remove"
+find "$httpdocroot"/cfr -type f -print | grep COPY | xargs -n1 rm >> /tmp/libsrshim.log.tmp 2>&1
+find "$httpdocroot"/cfr -type f -print | grep MOVE | xargs -n1 rm >> /tmp/libsrshim.log.tmp 2>&1
+find "$httpdocroot"/cfr -type f -print | grep LINK | xargs -n1 rm >> /tmp/libsrshim.log.tmp 2>&1
+find "$httpdocroot"/cfr -type l -print | grep LINK | xargs -n1 rm >> /tmp/libsrshim.log.tmp 2>&1
+wait_dir_to_be_the_same remove
+echo "success"
--- test/mirror_check.sh
+++ test/mirror_check.sh
@@ -19,10 +19,35 @@ print('export LOGDIR=%s'% logdir)
 EOF
 }
 
-# sr_post initial start
+eval `application_dirs`
+
 tstdir="`pwd`"
 httpdocroot=`cat $tstdir/.httpdocroot`
 
+
+# function to see if same amount of files
+
+function wait_dir_to_be_the_same() {
+
+       cd "$httpdocroot"/cfr
+       COUNT=`find . -type f -print | wc -l`
+       COUNT2=`find . -type l -print | wc -l`
+       TCOUNT=$(($COUNT+$COUNT2))
+       sleep 5
+       cd ../cfile
+       MCOUNT=`find . -type f -print | wc -l`
+       MCOUNT2=`find . -type l -print | wc -l`
+       TMCOUNT=$(($MCOUNT+$MCOUNT2))
+       while [ "${TMCOUNT}" != "${TCOUNT}" ]; do
+             sleep 5
+             MCOUNT=`find . -type f -print | wc -l`
+             MCOUNT2=`find . -type l -print | wc -l`
+             TMCOUNT=$(($MCOUNT+$MCOUNT2))
+             echo "(${TMCOUNT} expecting ${TCOUNT})"
+       done
+}
+
+# sr_post initial start
 # MIRROR TEST
 
 sr_cpost stop veille_f34 > /dev/null 2>&1
@@ -31,84 +56,63 @@ rm "$LOGDIR"/sr_cpost_veille_f34*.log 2> /dev/null
 sr_subscribe stop cfile_f44 > /dev/null 2>&1
 rm "$LOGDIR"/sr_subscribe_cfile_f44*.log 2> /dev/null
 
+# preventive cleanup (previous runs)
+
+cd "$httpdocroot"/cfr
+find . -type f -print | grep COPY | xargs -n1 rm 2> /dev/null
+find . -type l -print | grep LINK | xargs -n1 rm 2> /dev/null
+find . -type f -print | grep LINK | xargs -n1 rm 2> /dev/null
+find . -type f -print | grep MOVE | xargs -n1 rm 2> /dev/null
+
+cd "$httpdocroot"/cfile
+find . -type f -print | grep COPY | xargs -n1 rm 2> /dev/null
+find . -type l -print | grep LINK | xargs -n1 rm 2> /dev/null
+find . -type f -print | grep LINK | xargs -n1 rm 2> /dev/null
+find . -type f -print | grep MOVE | xargs -n1 rm 2> /dev/null
+
 sr_cpost start veille_f34 > /dev/null 2>&1
 sr_subscribe start cfile_f44 > /dev/null 2>&1
 
-# preventive cleanup (previous runs)
+# copy 
 
-cd "$httpdocroot"
+echo "checking sr_cpost copy"
+cd "$httpdocroot"/cfr
+find . -type f -print                | xargs -iAAA cp AAA AAA.COPY
+find . -type f -print | grep -v COPY | xargs -iAAA cp AAA AAA.COPY2
+wait_dir_to_be_the_same
+echo "success"
 
-cd cfr
-rm *.copy* *.softlink *.link *.move 2> /dev/null
-cd ..
+# move 
 
-cd cfile
-rm *.copy* *.softlink *.link *.move 2> /dev/null
-cd ..
+echo "checking sr_cpost move"
+cd "$httpdocroot"/cfr
+find . -type f -print | grep -v COPY | xargs -iAAA  mv AAA.COPY2 AAA.MOVE
+wait_dir_to_be_the_same
+echo "success"
 
-# copy, link, softlink
+# softlink 
 
-cd cfr
-ORIGINAL=`ls`
-for f in $ORIGINAL; do
-    cp "$f" "$f".copy
-    cp "$f" "$f".copy2
-    ln -s "$f" "$f".softlink
-    ln "$f" "$f".link
-done
-COUNT=`ls| wc -l`
+echo "checking sr_cpost softlink"
+cd "$httpdocroot"/cfr
+find . -type f -print | grep -v COPY | grep -v MOVE | xargs -iAAA  ln -s AAA AAA.SLINK
+wait_dir_to_be_the_same
+echo "success"
 
-# expect the same in subscribe
+# hardlink 
 
-cd ../cfile
-TARGET_COUNT=`ls| wc -l`
-while [ "${TARGET_COUNT}" != "${COUNT}" ]; do
-      sleep 5
-      TARGET_COUNT=`ls| wc -l`
-      echo "(${TARGET_COUNT} of ${COUNT}) (COPY,SOFTLINK,LINK)"
-done
-echo "SUCCESS (COPY, SOFTLINK, LINK)"
+echo "checking sr_cpost hardlink"
+cd "$httpdocroot"/cfr
+find . -type f -print | grep -v COPY | grep -v MOVE | grep -v LINK | xargs -iAAA ln AAA AAA.HLINK
+wait_dir_to_be_the_same
+echo "success"
 
+# hardlink 
 
-# move files to a new file
-
-cd ../cfr
-for f in $ORIGINAL; do
-    mv "$f".copy2 "$f".move
-done
-COUNT=`ls *.move| wc -l`
-sleep 2
-
-# expect the same in subscribe
-
-cd ../cfile
-TARGET_COUNT=`ls *.move| wc -l`
-while [ "${TARGET_COUNT}" != "${COUNT}" ]; do
-      sleep 5
-      TARGET_COUNT=`ls *.move| wc -l`
-      echo "(${TARGET_COUNT} of ${COUNT}) (MOVE)"
-done
-echo "SUCCESS (MOVE)"
-echo
-
-
-# remove copy, link, softlink
-
-cd ../cfr
-rm *.copy*
-rm *.softlink
-rm *.link
-rm *.move
-COUNT=`ls| wc -l`
-
-# expect the same in subscribe
-
-cd ../cfile
-TARGET_COUNT=`ls| wc -l`
-while [ "${TARGET_COUNT}" != "${COUNT}" ]; do
-      sleep 5
-      TARGET_COUNT=`ls| wc -l`
-      echo "(${TARGET_COUNT} of ${COUNT}) (REMOVE)"
-done
-echo "SUCCESS (REMOVE)"
-
+echo "checking sr_cpost remove"
+cd "$httpdocroot"/cfr
+find . -type f -print | grep COPY | xargs -n1 rm
+find . -type f -print | grep MOVE | xargs -n1 rm
+find . -type f -print | grep LINK | xargs -n1 rm
+find . -type l -print | grep LINK | xargs -n1 rm
+wait_dir_to_be_the_same
+echo "success"
