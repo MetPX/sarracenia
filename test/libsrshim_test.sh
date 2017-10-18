e76936194a5b6f15c244f81bc8884568afbfe91d
michel grenier <michel.grenier8@canada.ca>
2017-10-18 20:27:53 +0000

    MG turning libsrshim off when done

--- test/libsrshim_test.sh
+++ test/libsrshim_test.sh
@@ -120,3 +120,6 @@ find . -type f -print | grep LINK | xargs -n1 rm  >> /tmp/libsrshim.log.tmp 2>&1
 find . -type l -print | grep LINK | xargs -n1 rm  >> /tmp/libsrshim.log.tmp 2>&1
 wait_dir_to_be_the_same f
 echo "success"
+
+export SR_POST_CONFIG=""
+export LD_PRELOAD=""
