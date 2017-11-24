1d036d0fc9ea784c0386d0ac404ae2766a060447
michel grenier <michel.grenier8@canada.ca>
2017-11-24 14:34:15 +0000

    MG poll_pulse.py script... posting to all reasonable exchanges on broker

--- sarra/plugins/poll_pulse.py
+++ sarra/plugins/poll_pulse.py
@@ -5,23 +5,31 @@
 
  usage:
      in an sr_poll configuration file:
+     the post broker user must be the admin 
+     because we pulse on all exchanges
 
  do_poll poll_pulse.py
 
  options:
 
  pulse_message
-     a message to be logged to all sarracenia clients on that broker
+     a message to be logged to all sarracenia consuming messages
+     This message can be considered as a startup message
+     Might be usefull if message = 'pulse restarted'
 
- The PULSE_MESSAGE environment variable can be set and used 
- just sr_poll <config> restart  after setting it
+ environment variable:
 
- The message will be placed in the post message once
+ PULSE_MESSAGE : environment variable that can be set
+     to pulse a different message ... Just set this variable and
+     sr_poll <pulse_config> restart
 
- A default message can be set in the config with pulse_message
- Might be usefull if message = 'pulse restarted'
+ The code tries to minimize the posting to one time
+ But would repost on restart
+
+ Environment variable overwrites pulse_message option settings
 
 """
+
 
 class POLL_SCRIPT(object): 
 
@@ -40,8 +48,12 @@ class POLL_SCRIPT(object):
        self.last_pulse_message = None
           
    def perform(self,parent):
-       logger = parent.logger
-       msg    = parent.msg
+       logger      = parent.logger
+
+       self.logger = logger
+       self.parent = parent
+
+       msg = parent.msg
 
        # we should sleep at least 2 mins
 
@@ -53,19 +65,16 @@ class POLL_SCRIPT(object):
 
        try   :
                pulse_message = os.environ.get('PULSE_MESSAGE')
-               logger.info("env message = %s" % pulse_message)
        except: 
                if parent.pulse_message: pulse_message = parent.pulse_message
+
+       # try to post message once
 
        if pulse_message == self.last_pulse_message :
           pulse_message = None
 
-       # warn if not xpublic
+       # build message
 
-       if parent.post_exchange != 'xpublic' :
-          logger.warning("pulsing on %s" % parent.post_exchange)
-
-       msg.exchange = parent.post_exchange
        msg.topic    = 'v02.pulse'
 
        msg.set_time()
@@ -78,12 +87,60 @@ class POLL_SCRIPT(object):
        msg.headers['pulse'] = '%d' % parent.sleep
        msg.trim_headers()
 
-       msg.publish()
+       # pulse on all exchanges
+       # because of its topic, it should not impact any process
+       # that does not consider topic v02.pulse
+
+       lst_dict = self.rabbitmqadmin("list exchanges name")
+
+       ex = []
+       for edict in lst_dict :
+           exchange = edict['name']
+           if exchange == ''        : continue
+           if exchange[0] != 'x'    : continue
+           if exchange == 'xreport' : continue
+           # deprecated exchanges
+           if exchange == 'xlog'    : continue
+           if exchange[0:3] == 'xl_': continue
+           if exchange[0:3] == 'xr_': continue
+           ex.append(exchange)
+           msg.exchange = exchange
+           msg.publish()
+
+       if pulse_message :
+          logger.info("message pulsed to exchanges %s" % ex)
 
        if pulse_message: logger.info("pulse %s %s" % (msg.topic,pulse_message))
        else            : logger.info("pulse %s" % msg.topic)
 
        return False
+
+   # from sr_audit.py
+   def rabbitmqadmin(self,options):
+        try   : from sr_rabbit       import exec_rabbitmqadmin
+        except: from sarra.sr_rabbit import exec_rabbitmqadmin
+
+        self.logger.debug("sr_audit rabbitmqadmin %s" % options)
+        try :
+                 (status, answer) = exec_rabbitmqadmin(self.parent.post_broker,options,self.logger)
+                 if status != 0 or answer == None or len(answer) == 0 or 'error' in answer :
+                    self.logger.error("rabbitmqadmin invocation failed")
+                    return []
+
+                 if answer == None or len(answer) == 0 : return []
+
+                 lst = []
+                 try    : lst = eval(answer)
+                 except : pass
+
+                 return lst
+
+        except :
+                (stype, svalue, tb) = sys.exc_info()
+                self.logger.error("Type: %s, Value: %s,  ..." % (stype, svalue))
+                self.logger.error("rabbimtqadmin "+ options)
+        return []
+
 
 poll_script = POLL_SCRIPT(self)
 self.do_poll = poll_script.perform
