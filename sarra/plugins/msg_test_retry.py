eee51e98643c3e90267a2577682f9eb6480d36ba
michel grenier <michel.grenier8@canada.ca>
2017-12-01 16:54:22 +0000

    MG 50% failure plugin in subscribe

--- sarra/plugins/msg_test_retry.py
+++ sarra/plugins/msg_test_retry.py
@@ -4,6 +4,7 @@
                    a download or send error.
 
                    when a message is being retried, it is randomly fixed
+
 """
 
 import random
@@ -12,21 +13,34 @@ class Msg_test_retry():
 
     def __init__(self,parent):
         self.parent = parent
-        self.url_bad  = 'sftp://toto@titi'
-        self.url_good = None
+        self.msg_baseurl_bad     = 'ftps://retry_user:retry_passwd@retry_host'
+        self.msg_baseurl_good    = None
+        self.parent_details_good = None
+        self.parent_details_bad  = None
 
     def perform(self,parent):
         l = parent.logger
         m = parent.msg
 
-        if self.url_good == None :
-           self.url_good= m.baseurl
+        if self.msg_baseurl_good == None :
+           self.msg_baseurl_good = m.baseurl
+
+        if self.parent_details_good == None and hasattr(self.parent,'details') :
+           self.parent_details_good = parent.details
+           self.parent_details_bad  = parent.credentials.get(self.msg_baseurl_bad)
+           
+        parent.details = self.parent_details_good
         
         if m.isRetry and random.randint(0,2) :
-           m.baseurl = self.url_good
+           m.set_notice(self.msg_baseurl_good,m.relpath,m.time)
 
         elif random.randint(0,2) :
-           m.baseurl = self.url_bad
+           # if sarra or subscribe  break download
+           if parent.program_name != 'sr_sender' :
+              m.set_notice(self.msg_baseurl_bad, m.relpath,m.time)
+           # if sender break destination
+           else: 
+              parent.details = self.parent_details_bad
 
         return True
 
