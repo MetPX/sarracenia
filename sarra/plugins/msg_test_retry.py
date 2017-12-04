6bd9d5cbb0882135fe43c62b46e7e67da5da8488
michel grenier <michel.grenier8@canada.ca>
2017-12-04 18:31:39 +0000

    MG  msg_test_retry.py causing sender flow_test to have trouble sending

--- sarra/plugins/msg_test_retry.py
+++ sarra/plugins/msg_test_retry.py
@@ -4,44 +4,56 @@
                    a download or send error.
 
                    when a message is being retried, it is randomly fixed
-
 """
 
 import random
 
+try :    from sr_credentials       import *
+except : from sarra.sr_credentials import *
+
 class Msg_test_retry():
 
     def __init__(self,parent):
-        self.parent = parent
-        self.msg_baseurl_bad     = 'ftps://retry_user:retry_passwd@retry_host'
-        self.msg_baseurl_good    = None
-        self.parent_details_good = None
-        self.parent_details_bad  = None
+        self.destination      = None
+        self.msg_baseurl_good = None
+        self.details_bad      = None
+        self.msg_baseurl_bad  = 'sftp://ruser:rpass@retryhost'
 
     def perform(self,parent):
+
         l = parent.logger
         m = parent.msg
 
-        if self.msg_baseurl_good == None :
-           self.msg_baseurl_good = m.baseurl
+        l.debug("msg_test_retry")
 
-        if self.parent_details_good == None and hasattr(self.parent,'details') :
-           self.parent_details_good = parent.details
-           self.parent_details_bad  = parent.credentials.get(self.msg_baseurl_bad)
-           
-        parent.details = self.parent_details_good
-        
+        if self.destination      == None: self.destination = parent.destination
+
+        if self.msg_baseurl_good == None: self.msg_baseurl_good = m.baseurl
+
+        # 50% chance for retry message to recover (will try later)
         if m.isRetry and random.randint(0,2) :
+           parent.destination = self.destination
+           ok, parent.details = parent.credentials.get(self.destination)
            m.set_notice(self.msg_baseurl_good,m.relpath,m.time)
 
+        # original message :  50% chance of breakage
         elif random.randint(0,2) :
+
            # if sarra or subscribe  break download
            if parent.program_name != 'sr_sender' :
+              l.debug("making it bad 1")
+              ok, parent.details = parent.credentials.get(self.destination)
               m.set_notice(self.msg_baseurl_bad, m.relpath,m.time)
+
            # if sender break destination
            else: 
-              parent.details = self.parent_details_bad
+              l.debug("making it bad 2")
+              parent.sleep_connect_try_interval_max = 1.0
+              parent.destination = self.msg_baseurl_bad
+              parent.credentials.parse(self.msg_baseurl_bad)
+              ok, parent.details = parent.credentials.get(self.msg_baseurl_bad)
 
+        l.debug("return from msg_test_retry")
         return True
 
 msg_test_retry=Msg_test_retry(None)
