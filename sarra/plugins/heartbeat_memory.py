857ecea49a24ac4dfee2977400e95a8458926438
michel grenier <michel.grenier8@canada.ca>
2017-12-05 15:43:22 +0000

    MG heartbeat_memory.py first shot untested

---
+++ sarra/plugins/heartbeat_memory.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import psutil,subprocess
+
+"""
+  default on_heartbeat handler that restarts program 
+  when detecting a memory leak... Memory threshold is
+  set to 2 x the program memory size when 100 files
+  were processed 
+"""
+
+class Heartbeat_Memory(object): 
+
+    def __init__(self,parent):
+        self.threshold  = 0
+          
+    def perform(self,parent):
+        self.logger     = parent.logger
+
+        if parent.message_count < 100 : return True
+
+        # from doc memory_full_info()  # "real" USS memory usage (Linux, OSX, Win only)
+        # mem(rss=10199040, vms=52133888, shared=3887104, text=2867200, lib=0,\
+        #          data=5967872, dirty=0, uss=6545408, pss=6872064, swap=0)
+
+        p = psutil.Process(parent.pid)
+        mem = p.memory_info()
+
+        if self.threshold == 0 :
+           self.threshold = 10 * mem.vms
+           self.logger.info("memory threshold set to %d" % self.threshold)
+           return True
+
+        if mem.vms > self.threshold : self.restart()
+
+        return True
+
+    def restart(self,parent):
+        cmd = []
+        cmd.append(parent.program_name)
+
+        if parent.instance > 0 :
+           cmd.append("--no")
+           cmd.append("%d" % parent.instance)
+
+        if parent.user_args  and len(parent.user_args) > 0 : cmd.extend(parent.user_args)
+        if parent.user_config: cmd.append( parent.user_config )
+
+        cmd.append('restart')
+
+        parent.logger.debug("restart with %s" % cmd)
+        subprocess.check_call( cmd )
+        return
+
+heartbeat_memory = Heartbeat_Memory()
+self.on_heartbeat = heartbeat_memory.perform
