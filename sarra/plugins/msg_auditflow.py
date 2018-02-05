d9e0ba227a0bacb9f948224f2b2f606389adcf7b
michel grenier <michel.grenier8@canada.ca>
2018-02-05 22:20:00 +0000

    MG taking into account sr_subscribe cp_f61  (smc_download_cp test)

--- sarra/plugins/msg_auditflow.py
+++ sarra/plugins/msg_auditflow.py
@@ -39,7 +39,7 @@ class Msg_AuditFlow(object):
         parent.auditflow_Atotal+=1
         i=0
 
-        for d in [ "downloaded_by_sub_u", "sent_by_tsource2send", "posted_by_srpost_test2", "recd_by_srpoll_test1" ]:
+        for d in [ "downloaded_by_sub_u", "sent_by_tsource2send", "posted_by_srpost_test2", "recd_by_srpoll_test1", "downloaded_by_sub_cp" ]:
             i+=1
             b= "%s/%s/%s/%s" % ( parent.msg_auditflow_topdir, d, middir, msg.new_file )
             if os.path.exists(b):
@@ -61,7 +61,10 @@ class Msg_AuditFlow(object):
                  parent.logger.error("msg_auditflow: file could not be deleted because already gone: %s" % ( f ) ) 
             # sr_watch running here should propagate the deletion to the other directories.
         
-        tally = ( parent.auditflow_Atotal*parent.auditflow_BtoAratio  /  parent.auditflow_Bgood ) 
+        tally = 0
+        if parent.auditflow_Bgood > 0 :
+           tally = ( parent.auditflow_Atotal*parent.auditflow_BtoAratio  /  parent.auditflow_Bgood ) 
+
         if ( tally > 0.90 ) and ( tally < 1.1 ) : 
             so_far="GOOD" 
         else: 
