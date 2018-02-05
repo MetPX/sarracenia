6a648be92d5f0df96277e59ff50123637628012a
michel grenier <michel.grenier8@canada.ca>
2018-02-05 20:40:14 +0000

    MG trying to comment its purpose... Peter can revisit...    I am not a great writer.

--- sarra/plugins/msg_http_to_https.py
+++ sarra/plugins/msg_http_to_https.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
-import os,stat,time
+"""
+This plugin simply turns message with baseurl http://...  into   https://...
+Sample usage is to put this line anywhere in a configuration:
+
+on_message msg_http_to_https.py
+
+The process would need to be restarted. From now on, all http messages that would be
+consumed, would be turned into an https message. The remaining of the process will
+treat the message as if posted that way. That plugin is an easy way to turn transaction
+between dd.weather.gc.ca and the user  into  secured https transfers.
+
+"""
 
 class msg_http_to_https(object): 
 
