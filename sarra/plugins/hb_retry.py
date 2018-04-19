3106421023dfbc32c5342227083e42f6f4e28a04
michel grenier <michel.grenier8@canada.ca>
2018-04-19 18:44:28 +0000

    MG make sure retry heartbead does not depend on previous on_start plugins

--- sarra/plugins/hb_retry.py
+++ sarra/plugins/hb_retry.py
@@ -3,23 +3,16 @@
 class RETRY(object): 
 
    def __init__(self,parent):
-       self.retry = None
-
-   def on_start(self,parent):
-       parent.logger.info("hb_retry on_start")
-
-       if not hasattr(parent,'consumer'): return
-       if not hasattr(parent.consumer, "retry" ): return
-
-       self.retry = parent.consumer.retry
-       self.retry.on_heartbeat(parent)
-
-       return True
+       pass
 
    def on_heartbeat(self,parent):
        parent.logger.info("hb_retry on_heartbeat")
 
-       if self.retry : self.retry.on_heartbeat(parent)
+       if not parent.retry_mode : return True
+
+       if not hasattr(parent,'consumer'): return True
+
+       parent.consumer.retry.on_heartbeat(parent)
 
        return True
 
