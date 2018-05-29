3ae5fca40a71f8451229d2e87c3091ab4ae0d506
Peter Silva <petersilva@sourceforge.net>
2018-05-29 11:45:00 -0400

    We are so late in beta, it is more production.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
               ]
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Intended Audience :: System Administrators',
