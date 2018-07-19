d7a9bcbd15e3a58a21bf503482fc07605fd98e64
Peter Silva <petersilva@sourceforge.net>
2018-07-19 16:24:49 -0400

    oops, merge issue.

--- sarra/sr_config.py
+++ sarra/sr_config.py
@@ -1089,8 +1089,8 @@ class sr_config:
     def run_command(self,cmd_list):
         sr_path = os.environ.get('SARRA_LIB')
         sc_path = os.environ.get('SARRAC_LIB')
-        try   :
         import sys,subprocess
+
         try:
                 if sys.version_info.major < 3 or (sys.version_info.major == 3 and sys.version_info.minor < 5) :
                         subprocess.check_call(cmd_list)
