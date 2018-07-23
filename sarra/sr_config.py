a1b3ce95e282bbd246bcd2174d9dae7961932a0f
petersilva <petersilva@sourceforge.net>
2018-07-22 23:10:24 -0400

    add close_fds=False to older subprocess calls as well.  Should go back to python 3.2

--- sarra/sr_config.py
+++ sarra/sr_config.py
@@ -1090,7 +1090,7 @@ class sr_config:
         import sys,subprocess
         try:
                 if sys.version_info.major < 3 or (sys.version_info.major == 3 and sys.version_info.minor < 5) :
-                        subprocess.check_call(cmd_list)
+                        subprocess.check_call(cmd_list, close_fds=False )
                 else :
                         subprocess.run(cmd_list, check=True, close_fds=False )
 
