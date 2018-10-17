10a9337071e279c54abfcee08bb6bab699fe562d
mjip <marie.payne@canada.ca>
2018-10-17 14:07:31 -0400

    added support for delete config option, parsing msg notice

--- sarra/plugins/download_email_ingest.py
+++ sarra/plugins/download_email_ingest.py
@@ -38,8 +38,10 @@ class Fetcher(object):
                 import poplib, imaplib, datetime, logging, email
 
                 logger = parent.logger                
+                deleteflg = parent.delete
+                logger.info("delete FLAG sdfjasndjkgnaskjdfgndsfg %s" % deleteflg)
 
-                ok, details = parent.credentials.get(parent.destination)
+                ok, details = parent.credentials.get(parent.msg.notice.split()[1])
                 if ok: 
                         setting         = details.url
                         user            = setting.username
@@ -48,7 +50,7 @@ class Fetcher(object):
                         protocol        = setting.scheme.lower() 
                         port            = setting.port
                 else:
-                        logger.error("download_email_ingest: destination has invalid credentials: %s" % parent.destination)
+                        logger.error("download_email_ingest: destination has invalid credentials: %s" % parent.msg.notice.split()[1])
                         return False
 
                 if not port:
@@ -87,10 +89,13 @@ class Fetcher(object):
                                 logger.info("download_email_ingest downloaded file: %s" % parent.msg.new_dir+'/'+parent.msg.new_file)
                                 with open(parent.msg.new_dir+'/'+parent.msg.new_file, 'w') as f:
                                        f.write(msg)
-                                return True
+                                if deleteflg == "True":
+                                       mailman.store(index, '+FLAGS', '\\Deleted')
 
+                        mailman.expunge()
                         mailman.close()
                         mailman.logout()
+                        return True
                         
                 elif "pop" in protocol:
                         if protocol == "pops":
@@ -121,9 +126,11 @@ class Fetcher(object):
                                 logger.info("download_email_ingest downloaded file: %s" % parent.msg.new_dir+'/'+parent.msg.new_file)
                                 with open(parent.msg.new_dir+'/'+parent.msg.new_file, 'w') as f:
                                         f.write(msg)
-                                return True
 
+                                if deleteflg == "True":
+                                        mailman.dele(index+1)
                         mailman.quit()
+                        return True
 
                 else:
                         logger.error("download_email_ingest destination protocol must be one of 'imap/imaps' or 'pop/pops'.")
