e704cd0235c2b6833c49874ab70cbbe8a0b11beb
mjip <marie.payne@canada.ca>
2018-10-31 12:58:01 -0400

    initial commit for sr_subscribe xattr support #54

--- sarra/sr_message.py
+++ sarra/sr_message.py
@@ -102,7 +102,7 @@ class sr_message():
         if the file advertised is newer than the local one, and it has a different checksum, return True.
 
         """
-        fname = "%s%s%s" %  (self.new_dir, '/', self.new_file )
+        fname = os.path.join(self.new_dir, self.new_file)
         self.logger.debug("sr_message content_match %s" % (fname ) )
         
         self.local_checksum = None
@@ -143,6 +143,17 @@ class sr_message():
 
     def compute_local_checksum(self):
         self.logger.debug("sr_message compute_local_checksum new_dir=%s, new_file=%s" % ( self.new_dir, self.new_file ) )
+
+        try:
+              attr = xattr.xattr(os.path.join(self.new_dir, self.new_file))
+              if 'user.sr_sum' in attr:
+                    self.logger.debug("checksum extracted using xattr")
+                    self.local_checksum = attr['user.sr_sum'].decode("utf-8")
+                    return
+        except:
+              pass
+
+        self.logger.debug("checksum extracted by reading file/calculating")
 
         bufsize = self.bufsize
         if self.length < bufsize : bufsize = self.length
