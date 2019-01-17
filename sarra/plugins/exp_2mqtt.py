36013a376a2e8b75e91e4497d60f3f41ab8b8df1
petersilva <petersilva@sourceforge.net>
2019-01-16 20:09:33 -0500

    Added authentication support and info to exp_2mqtt.py publisher. Added tools/paho_sub_v03_sample.py really simple client.

--- sarra/plugins/exp_2mqtt.py
+++ sarra/plugins/exp_2mqtt.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
 
-exp_2mqtt - posts messages to an MQTT broker as they are received (like sr_shovel)
+exp_2mqtt  - posts messages to an MQTT broker as they are received (like sr_shovel)
 
 STATUS: Experimental (may change at any upgrade, do not use if stability is desired.)
-        This is just for exploration of interop with MQTT brokers. 
-
-        NOT FOR PRODUCTION!  NO TESTING so far.
+        This is just for exploration of interop with MQTT brokers. NOT FOR PRODUCTION!
+        NO TESTING so far.
 
         messages are posted in v03 format, because v02 format is incompatible with
         MQTT 3.11  (requires at least v5, which isn't common yet.)
 
-        developed posting to EMQ broker so far without authentication.
+        tested with EMQ so far, authenticated to post, subscribe anonymous
+        see after code for details.
 
-TODO:
-        try mosquitto broker.
-        add authentication.
-        understand reliability etc.
-
+This plugin posts messages to an MQTT broker as they are received (like sr_shovel)
 
 requires:
    paho-mqtt library.
@@ -69,13 +65,19 @@ class EXP_2MQTT(object):
          logger.error("exp_2mqtt: defaulting post_exchange to xpublic")
          parent.post_exchange='xpublic'
          
-      if parent.mqtt_bs.url.port == None:
+      u = parent.mqtt_bs.url
+      if u.port == None:
             port = 1883
       else:
-            port = parent.mqtt_bs.url.port
+            port = u.port
 
       parent.mqtt_client = mqtt.Client()
-      parent.mqtt_client.connect( parent.mqtt_bs.url.hostname, port )
+
+      if u.username != None:
+          logger.error("exp_2mqtt: authenticating as %s " % (u.username) )
+          parent.mqtt_client.username_pw_set( u.username, u.password )
+
+      parent.mqtt_client.connect( u.hostname, port )
       return True
 
    def on_message(self,parent):
@@ -89,7 +91,7 @@ class EXP_2MQTT(object):
 
       mqtt_topic = parent.post_exchange + '/v03/post' + os.path.dirname(parent.msg.relpath)
       body = json.dumps( (msg.pubtime, msg.baseurl, msg.relpath, msg.headers) )
-      logger.debug("exp_2mqtt publising topic=%s, body=%s" % ( mqtt_topic, body ))
+      logger.info("exp_2mqtt publising topic=%s, body=%s" % ( mqtt_topic, body ))
       parent.mqtt_client.publish(mqtt_topic,body)
 
       return True
@@ -98,3 +100,12 @@ class EXP_2MQTT(object):
 self.plugin='EXP_2MQTT'
 
 
+"""
+  {allow, all, subscribe, "xpublic/#" }
+  {allow, {user, "tsource"}, publish, [ "xpublic/#" ] }.
+
+  ...
+
+  {deny, all}.
+
+"""
