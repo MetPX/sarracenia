6aef2dd0a0fad0c160e2e9180cf9e2eefb799db6
Benoit Lapointe <benoit.lapointe@canada.ca>
2019-01-23 14:47:34 -0500

    Insured to run on more binaries as possible for better performance (in theory)

--- gen_win_install.sh
+++ gen_win_install.sh
@@ -7,10 +7,18 @@ mkdir pynsist_pkgs
 cd pynsist_pkgs/
 
 # Ensure that pip3 is always used to download-only windows whls
-pip3 download pika --only-binary=:all: --platform win_amd64
+pip3 download appdirs --only-binary=:all: --platform win_amd64
 pip3 download netifaces --only-binary=:all: --platform win_amd64
+pip3 download pika --only-binary=:all: --platform win_amd64
 pip3 download psutil --only-binary=:all: --platform win_amd64
 pip3 download paramiko --only-binary=:all: --platform win_amd64
+
+# No binary available, thats why they are not fetched by pip (to be tested later) 
+#pip3 download humanize --only-binary=:all: --platform win_amd64
+#pip3 download pycparser --only-binary=:all: --platform win_amd64
+#pip3 download pathtools --only-binary=:all: --platform win_amd64
+#pip3 download watchdog --only-binary=:all: --platform win_amd64
+
 cd ..
 
 # Remove last build
