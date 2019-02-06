89189d7828369ddd090617a8b8ac05d08569a13e
benlapETS <benoit.lapointe.3@etsmtl.net>
2019-02-06 17:54:35 -0500

    Why did I do that, must specify that its python3

--- generate-win-installer.sh
+++ generate-win-installer.sh
@@ -7,12 +7,12 @@ mkdir pynsist_pkgs
 cd pynsist_pkgs/
 
 # Ensure to download only windows binaries
-pip download amqp --only-binary=:all: --platform win_amd64
-pip download appdirs --only-binary=:all: --platform win_amd64
-pip download netifaces --only-binary=:all: --platform win_amd64
-pip download pika --only-binary=:all: --platform win_amd64
-pip download psutil --only-binary=:all: --platform win_amd64
-pip download paramiko --only-binary=:all: --platform win_amd64
+pip3 download amqp --only-binary=:all: --platform win_amd64
+pip3 download appdirs --only-binary=:all: --platform win_amd64
+pip3 download netifaces --only-binary=:all: --platform win_amd64
+pip3 download pika --only-binary=:all: --platform win_amd64
+pip3 download psutil --only-binary=:all: --platform win_amd64
+pip3 download paramiko --only-binary=:all: --platform win_amd64
 
 # No binary available, thats why they are not fetched by pip (to be tested later) 
 #pip3 download humanize --only-binary=:all: --platform win_amd64
@@ -25,7 +25,7 @@ cd ..
 # Remove last build
 rm -rf dist
 rm -rf build
-python setup.py bdist_wheel
+python3 setup.py bdist_wheel
 
 # NSIS packaging
 pynsist win_installer.cfg
