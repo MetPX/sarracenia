ef7c07c6eef226ceb3ff592c4efe9422be98a6aa
petersilva <petersilva@sourceforge.net>
2019-02-16 23:04:49 -0500

    exp_2mqtt.py ... arg... no line feed at end of integrity checksum.

--- sarra/plugins/exp_2mqtt.py
+++ sarra/plugins/exp_2mqtt.py
@@ -114,7 +114,7 @@ class EXP_2MQTT(object):
       
       sum_algo_map = { "d":"md5", "s":"sha512", "n":"md5name", "0":"zero" }
       sm = sum_algo_map[ msg.headers["sum"][0] ]
-      sv = encode( decode( msg.headers["sum"][2:], 'hex'), 'base64' ).decode('utf-8')
+      sv = encode( decode( msg.headers["sum"][2:], 'hex'), 'base64' ).decode('utf-8').strip()
       msg.headers[ "integrity" ] = { "method": sm, "value": sv }
       body = json.dumps( msg.headers )
       logger.info("exp_2mqtt publishing topic=%s, body=%s" % ( mqtt_topic, body ))
