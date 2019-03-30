37d867481b95091848357820f746d6e75894de49
petersilva <petersilva@sourceforge.net>
2019-03-30 16:24:48 -0400

    added more options for logging of posted messages.

--- sarra/plugins/post_log.py
+++ sarra/plugins/post_log.py
@@ -4,18 +4,30 @@
   default on_file handler logs that the file has been posted.
   prints a simple notice.
 
+  post_log_format raw|old|compact
 """
 
 class Post_Log(object): 
 
     def __init__(self,parent):
+        parent.declare_option('post_log_format')
         parent.logger.debug( "post_log initialized" )
+        self.post_log_format=[ 'old' ]
           
     def perform(self,parent):
 
         msg = parent.msg
-        parent.logger.info("post_log notice=%s %s %s headers=%s" % \
-            ( msg.pubtime, msg.baseurl, msg.relpath, msg.headers ) )
+        f = parent.post_log_format
+
+        if 'compact' in f :
+            parent.logger.info("post_log %s %s lag=%s baseurl=%s relpath=%s " % \
+                ( 'compact', msg.pubtime, msg.get_elapse(), msg.baseurl, msg.relpath ) )
+        elif 'v03' in f:
+            parent.logger.info("post_log %s %s lag=%s headers=%s " % \
+                ( 'v03', msg.pubtime, msg.get_elapse(), msg.headers ) )
+        elif 'old' in f:
+            parent.logger.info("post_log notice=%s %s %s headers=%s" % \
+                ( msg.pubtime, msg.baseurl, msg.relpath, msg.headers ) )
 
         return True
 
