99cfda0fc18efbdae32d445394c5c0105824fd4b
petersilva <petersilva@sourceforge.net>
2019-08-24 03:28:47 -0400

    Added config parsing, fixed problem with state parsing for C state files.

--- sarra/sr2.py
+++ sarra/sr2.py
@@ -27,6 +27,20 @@ def ageoffile(lf):
     """
     return(0);
 
+def _parse_cfg(cfg):
+    """ return configuration file as a dictionary.
+        FIXME: this is extremely rudimentary, doesn't do variable substitution, etc...
+               only want to use this to get 'instances' for now which should be ok 99% of the time.
+    """
+    cfgbody={}
+    for l in open(cfg,"r").readlines():
+       line = l.split()
+       if (len(line) < 1 ) or (  line[0] == '#' ):
+          continue
+       cfgbody[line[0]] = line[1:] 
+    return cfgbody
+
+
 class sr_GlobalState:
 
     """
@@ -75,9 +89,13 @@ class sr_GlobalState:
                    if state != 'unknown':
                        self.configs[c][cbase] = {}
                        self.configs[c][cbase]['state'] = state 
+                       cfgbody = _parse_cfg( cfg )
+                       if 'instances' in cfgbody:                        
+                           self.configs[c][cbase]['instances'] = cfgbody['instances'] 
+
                os.chdir('..')
    
-
+    
     def _read_states(self):
         # read in state files
         os.chdir(self.user_cache_dir)
@@ -94,16 +112,23 @@ class sr_GlobalState:
                        self.states[c][cfg]['instance_pids']={}
                        self.states[c][cfg]['queue_name']=None
                        self.states[c][cfg]['instances_expected']=0
+                       self.states[c][cfg]['has_state']=False
+
+                       #print( 'state %s/%s' % ( c, cfg ) )
                        for f in os.listdir():
-                            t = pathlib.Path(f).read_text()
+                            t = pathlib.Path(f).read_text().strip()
+                           
+                            #print( 'read f:%s len: %d contents:%s' % ( f, len(t), t[0:10] ) )
                             if len(t) == 0:
                                 continue
 
+                            #print( 'read f[-4:] = +%s+ ' % ( f[-4:] ) )
                             if f[-4:] == '.pid':
                                 i = int(f[-6:-4])
                                 if t.isdigit():
+                                    #print( "%s/%s instance: %s, pid: %s" % 
+                                    #     ( c, cfg, i, t ) )
                                     self.states[c][cfg]['instance_pids'][i]= int( t )
-                                    self.states[c][cfg]['instances_expected'] += 1
                             elif f[-6:] == '.qname' :
                                 self.states[c][cfg]['queue_name'] = t
                             elif f[-6:] == '.state' and ( f[-12:-6] != '.retry' ):
@@ -165,6 +190,8 @@ class sr_GlobalState:
     def _resolve(self):
         """
            compare configs, states, & logs and fill things in.
+
+           things that could be identified: differences in state, running & configured instances.
         """
 
         # comparing states and configs to find missing instances, and correct state.
@@ -178,14 +205,17 @@ class sr_GlobalState:
                   continue
                if len(self.states[c][cfg]['instance_pids']) > 0:
                   self.states[c][cfg]['missing_instances'] = []
-                  expected_instances=0
+                  observed_instances=0
                   for i in self.states[c][cfg]['instance_pids']:
                       if self.states[c][cfg]['instance_pids'][i] not in self.procs:
                          self.states[c][cfg]['missing_instances'].append(i) 
                       else:
-                         expected_instances+=1
+                         observed_instances+=1
                          self.procs[ self.states[c][cfg]['instance_pids'][i] ]['claimed'] = True
-                  if expected_instances < self.states[c][cfg]['instances_expected']:
+
+                  if observed_instances < self.states[c][cfg]['instances_expected']:
+                      print( "%s/%s observed_instances: %s expected: %s" % \
+                         ( c, cfg, observed_instances, self.states[c][cfg]['instances_expected'] ) )
                       self.configs[c][cfg]['state'] = 'partial'
                   else:
                       self.configs[c][cfg]['state'] = 'running'
@@ -208,7 +238,6 @@ class sr_GlobalState:
         self._read_states() 
         self._read_logs() 
         self._resolve()
-
 
     def stop(self):
 
