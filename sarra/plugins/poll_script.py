e09635ed586ceffb56d8d6693391024db0f94e58
Benoit Lapointe <benoit.lapointe@canada.ca>
2019-09-10 12:24:48 -0400

    Remove pipes from Popen calls in plugins

--- sarra/plugins/poll_script.py
+++ sarra/plugins/poll_script.py
@@ -62,7 +62,7 @@ class POLL_SCRIPT(object):
       
       # run the shell script, for loop processes each line of output 
       #      as an absolute path to a file name.
-      proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+      proc = subprocess.Popen(cmd)
       
       for line in proc.stdout:
           fname = line.rstrip().decode("utf-8") 
