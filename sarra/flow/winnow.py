113de673fdbb39c4a4da4dd3f4046953d72cec04
PeterSilva <peter@bsqt.homeip.net>
2020-05-23 22:54:32 -0400

    um... yeah...

---
+++ sarra/flow/winnow.py
@@ -0,0 +1,37 @@
+
+import sarra.moth
+import copy
+from sarra.flow import Flow
+import logging
+
+logger = logging.getLogger( '__name__' )
+
+
+class Winnow(Flow):
+
+     default_options = { 
+         'download' : False, 
+         'accept_unmatched': True, 
+         'suppress_duplicates': 300
+     }
+
+     @classmethod
+     def assimilate(cls,obj):
+         obj.__class__ = Shovel
+
+     def name(self):
+         return 'winnow'
+
+     def __init__( self ):
+
+         Winnow.assimilate(self)
+
+     def do( self ):
+         pass
+
+     def report( self ):
+         """
+         winnows inherently do not do anything substantive to a file, so suppress reports.
+         """
+         pass
+
