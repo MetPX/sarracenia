c3ae2a8af4cd243ce38eccaea385cdfd9c7e5bb1
Tyson Kaufmann <kaufmann91@gmail.com>
2020-07-20 12:42:35 -0700

    Fix TravisCI debian package installs

--- travis/install.sh
+++ travis/install.sh
@@ -2,7 +2,6 @@
 #
 # Script should be called from root directory
 
-sudo apt -y install git devscripts debhelper dh-python python3-setuptools python3-docutils python3-amqp python3-appdirs python3-humanize python3-psutil python3-watchdog
 debuild -us -uc
 sudo dpkg -i ../metpx-sarracenia*.deb
 git clone https://github.com/MetPX/sr_insects ${HOME}/sr_insects
