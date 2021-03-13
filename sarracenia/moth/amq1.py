2f2fb7e3080aff3b3ad806dbf6f303a99fa74306
PeterSilva <peter@bsqt.homeip.net>
2021-03-13 10:31:00 -0500

    hint to implement amq1 with proton library.

--- sarracenia/moth/amq1.py
+++ sarracenia/moth/amq1.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 class AMQ1(Moth):
     def __init__(self, broker, props, is_subscriber):
         """
-
+            AMQP 1.0 library to be built with libqpid-proton (the only free amqp 1.0 library around.)
         """
         super().__init__(broker, props, is_subscriber)
 
