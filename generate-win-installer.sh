a9bd3e26b6de4e2380f87010c6dd223ec261e6b2
PeterSilva <peter@bsqt.homeip.net>
2021-03-28 19:51:57 -0400

    to create windows packages from ubuntu 20.04, need pysnsist 2.7, and netifaces isn't available for python3.8 pick an alternate source. normal source is commented (need to use that on ubuntu 18.)

--- generate-win-installer.sh
+++ generate-win-installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 
 # Windows specific pkgs
 rm -rf pynsist_pkgs
@@ -9,7 +9,8 @@ cd pynsist_pkgs/
 # Ensure to download only windows binaries
 pip3 download amqp --only-binary=:all: --platform win_amd64
 pip3 download appdirs --only-binary=:all: --platform win_amd64
-pip3 download netifaces --only-binary=:all: --platform win_amd64
+#pip3 download netifaces --only-binary=:all: --platform win_amd64
+pip3 download netifaces-w38 --only-binary=:all: --platform win_amd64
 pip3 download pika --only-binary=:all: --platform win_amd64
 pip3 download psutil --only-binary=:all: --platform win_amd64
 pip3 download paramiko --only-binary=:all: --platform win_amd64
