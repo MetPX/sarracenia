8cb106372f2ca93550ecf2fe602ccd94ee6ed252
PeterSilva <peter@bsqt.homeip.net>
2021-05-24 19:38:55 -0400

    Add_sr3 added to travis setup scripts.

---
+++ travis/add_sr3.sh
@@ -0,0 +1,16 @@
+# Flow Test Add sr3
+# given a test vm, add v03 versions on top, to be able to test them.
+
+# Install and configure dependencies
+sudo apt -y install metpx-sr3c
+
+# to be able to build v3 packages:
+# assume on v03_wip branch... (git checkout v03_wip)
+sudo apt -y install devscripts
+# needs to be run from root dir of cloned rep.
+sudo apt -y build-dep .
+debuild -us -uc
+dpkg -i ../metpx-sr*.deb
+#missing deps result...
+sudo apt -y install -f 
+
