d08725785708da5452e27a149db19f1b77c75abb
PeterSilva <peter@bsqt.homeip.net>
2021-05-26 09:55:13 -0400

    updates for installing outside travis (but with raw vm images using multipass)

--- travis/install.sh
+++ travis/install.sh
@@ -2,7 +2,10 @@
 #
 # Script should be called from root directory
 
+sudo apt update
 sudo apt install -y git python3-pip
+
+export PATH=${PATH}:${HOME}/.local/bin
 pip3 install -U pip
 pip3 install -e .
 git clone https://github.com/MetPX/sr_insects ${HOME}/sr_insects
