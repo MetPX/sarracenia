ad292ea4d62692a5e5b80ce5fd961f78165f72a8
PeterSilva <peter@bsqt.homeip.net>
2021-11-01 17:53:41 -0400

    holy crap alternate key generation algorithms were omitted in nodupe.py

---
+++ sarracenia/flowcb/nodupe/name.py
@@ -0,0 +1,20 @@
+
+import logging
+from sarracenia.flowcb import FlowCB
+
+logger = logging.getLogger(__name__)
+
+
+
+class Name(FlowCB):
+    def __init__(self, options):
+        self.o = options
+
+    def after_accept(self, worklist ):
+        for m in worklist.incoming:
+            if not 'nodupe_override' in m:
+                m['_deleteOnPost'] |= set( [ 'nodupe_override' ] )
+                m['nodupe_override'] = {}
+ 
+            m['nodupe_override']['path'] = m['relPath'].split('/')[-1]
+
