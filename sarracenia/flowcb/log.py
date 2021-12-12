38c355bc350e4e509c50af5c2bbac03949160903
PeterSilva <peter@bsqt.homeip.net>
2021-12-12 01:14:18 -0500

    updating dd_ping for sr3, also adding stats to the log callback.

--- sarracenia/flowcb/log.py
+++ sarracenia/flowcb/log.py
@@ -1,4 +1,5 @@
 import logging
+from sarracenia import nowflt, timestr2flt
 from sarracenia.flowcb import FlowCB
 
 logger = logging.getLogger(__name__)
@@ -18,6 +19,12 @@ class Log(FlowCB):
         self.o = options
         self.o.add_option( 'logEvents', 'set', [ 'after_accept' ] )
         logger.info( 'initialized with: %s' % self.o.logEvents )
+        self.file_bytes=0
+        self.lag_cumulative=0 
+        self.lag_max=0 
+        self.message_bytes=0
+        self.message_count=0
+        self.reject_count=0
 
     def gather(self):
         if set ( ['gather', 'all'] ) & self.o.logEvents:
@@ -27,6 +34,10 @@ class Log(FlowCB):
 
     def after_accept(self, worklist):
 
+        self.reject_count += len(worklist.rejected)
+        self.message_count += len(worklist.incoming)
+        now=nowflt()
+
         if set ( ['reject', 'all'] ) & self.o.logEvents:
             for msg in worklist.rejected:
                 if 'report' in msg:
@@ -34,9 +45,19 @@ class Log(FlowCB):
                 else:
                     logger.info("rejected: %s " % msg.dumps() )
 
-        if set ( ['after_accept', 'all'] ) & self.o.logEvents:
-            for msg in worklist.incoming:
-                logger.info("accepted: %s " % msg.dumps() )
+        for msg in worklist.incoming:
+
+            lag = now - timestr2flt(msg['pubTime'])
+            self.lag_cumulative += lag
+            if lag > self.lag_max:
+               self.lag_max = lag
+            self.message_bytes += len(msg)
+            if 'size' in msg:
+                self.file_bytes += msg['size']
+
+            if set ( ['after_accept', 'all'] ) & self.o.logEvents:
+                logger.info("accepted: (lag: %g ) %s " % ( lag, msg.dumps() ) )
+                
 
     def after_work(self, worklist):
         if set ( ['reject', 'all'] ) & self.o.logEvents:
@@ -50,17 +71,26 @@ class Log(FlowCB):
             for msg in worklist.ok:
                 logger.info("worked successfully: %s " % msg.dumps() )
 
+    def _stats(self):
+        logger.info( "messages_received: %d, accepted: %d, rejected: %d ( bytes: %s )" %
+            ( self.message_count+self.reject_count, self.message_count, self.reject_count, self.message_bytes ) )
+        logger.info( "files_accepted: cumulative bytes: %d" % self.file_bytes )  
+        if self.message_count > 0:
+            logger.info( "lag: average: %g, maximum: %g " % ( self.lag_cumulative/self.message_count, self.lag_max ) )
 
     def on_stop(self):
         if set ( ['on_stop', 'all'] ) & self.o.logEvents:
+            self._stats()
             logger.info("stopping")
 
     def on_start(self):
         if set ( ['on_start', 'all'] ) & self.o.logEvents:
+            self._stats()
             logger.info("starting")
 
     def on_housekeeping(self):
         if set ( ['on_housekeeping', 'all'] ) & self.o.logEvents:
+            self._stats()
             logger.info("housekeeping")
 
     def post(self, worklist):
