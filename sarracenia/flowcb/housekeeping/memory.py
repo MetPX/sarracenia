a0485d3a0d13aee2528600088846af43d3c75a6c
MagikEh <MagikEh@gmail.com>
2022-01-19 14:39:51 -0600

    Get housekeeping/memory plugin mostly working Still needs to be tested to see if Windows properly supports execl for 'restarting' the process in place.
    Would also be nice to have a test suite that purposely blows up memory
    footprint, currently only watch/f40 has a flag for memMultiplier=1.02
    that /may/ trigger the restart.
    But we need to verify that python's execl actually reclaims memory when
    executing in place. (it at least preserves PIDs) #395

--- sarracenia/flowcb/housekeeping/memory.py
+++ sarracenia/flowcb/housekeeping/memory.py
@@ -2,41 +2,47 @@
 """
 Default `on_heartbeat` handler that restarts components to deal with memory leaks.
 
-The program processes the first `hkMemoryBaseLineFile` items
+Setting `MemoryMax` in the config overrides the following:
+
+The plugin processes the first `MemoryBaseLineFile` items to reach a steady state before setting the threshold
 (messages-in for subscribers, messages-posted for posting programs).
-Once processed, the `memory max threshold` is set to `memory_multiplier` times the current memory in use.
-If memory use ever exceeds the max, then the plugin triggers a restart, which should reduce the memory consumption.
+Once processed, the `MemoryMax` threshold is set to `MemoryMultiplier` * current memory in use.
+
+If memory use ever exceeds the `MemoryMax` threshold, then the plugin triggers a restart, which should reduce the memory consumption.
+
+
 
 Parameters
 ----------
 
-hkMemoryMax : size (default: infinity)
-    Hard coded maximum tolerable memory consumption.
+MemoryMax : size (default: none)
+    Hard coded maximum for tolerable memory consumption.
     Must be suffixed with k/m/g for Kilo/Mega/Giga byte values.
-    If there is no max given, then `hkMemoryMax` will be set to:  `<baseline_memory> * <multiplier>`
-    The following options will have an effect:
+    If not set then the following options will have an effect:
 
-hkMemoryBaseLineFile : int, optional (default: 100)
+MemoryBaseLineFile : int, optional (default: 100)
     How many files to process before measuring to establish the baseline memory usage.
+    (how many files are expected to process before a steady state is reached)
 
-hkMemoryMultiplier : int, optional (default: 3)
-    How much you want to allow the component to grow before you call it a memory leak.
+MemoryMultiplier : int, optional (default: 3)
+    How many times past the steady state memory footprint you want to allow the component to grow before considering it a memory leak.
     It could be normal for memory usage to grow, especially if plugins store data in memory.
 
 
 Returns
 -------
-    Nothing, restarts components if memory usage is outside of set thresholds.
+    Nothing, restarts components if memory usage is outside of configured thresholds.
 """
 
-from _typeshed import Self
 import logging
+from pickle import TRUE
 import humanize
 import os
 import psutil
 from sarracenia.config import chunksize_from_str
 from sarracenia.flowcb import FlowCB
-from sr import sr_GlobalState
+from sarracenia.sr import sr_GlobalState
+import sys
 
 logger = logging.getLogger(__name__)
 
@@ -45,91 +51,80 @@ class Memory(FlowCB):
 
     def __init__(self, options):
         self.o = options
-        self.o.add_option('hkMemoryMax', 'size')
-        self.o.add_option('hkMemoryBaselineFile', 'count', 100)
-        self.o.add_option('hkMemoryMultiplier', 'count', 3)
+        # Set option to neg value to determine if user set in config
+        self.o.add_option('MemoryMax', 'size', '-1k')
+        self.o.add_option('MemoryBaseLineFile', 'count', 100)
+        self.o.add_option('MemoryMultiplier', 'float', 3)
 
         self.threshold = None
-        self.file_count = None
+        ''' Per-process maximum memory footprint that is considered too large, forcing a process restart.'''
+        self.transferCount = 0
+        self.msgCount = 0
 
     def on_housekeeping(self):
-        logger.error('*Screams into the void\n\n\t\tAAAAHHHHHHHH\n\n...*')
-        logger.info('*Screams into the void\n\n\t\tAAAAHHHHHHHH\n\n...*')
-        self.checkMemory(self)
-
-    def checkMemory(self):
-        proc = psutil.Process()
-        if hasattr(proc, 'memory_info'):
-            # “Virtual Memory Size”, this is the total amount of virtual memory used by the process in bytes.
-            mem = proc.memory_info().vms
-        else:
-            # FIXME Is this a legacy smell? psutil Doesn't have this function now..
-            mem = proc.get_memory_info().vms
-
+        logger.info(f"According to sys.argv:\n\t{sys.argv}")
+        mem = psutil.Process().memory_info().vms
+        # TODO: Should this be instead in logger? Why does it exist here?
+        # Change this class to resources so it encapsulates more appropriately.
         ost = os.times()
-        logger.info("Memory cpu_times user=%s system=%s elapse=%s" %
-                    (ost.user, ost.system, ost.elapsed))
+        logger.info(f"Current Memory cpu_times: user={ost.user} system={ost.system} elapse={ost.elapsed}")
 
-        if self.file_count is None:
-            if hasattr(self.o, 'hkMemoryBaseLineFile'):
-                self.file_count = int(self.o.hkMemoryBaseLineFile)
-            else:
-                self.file_count = 100
-
+        # We must set a threshold **after** the config file has been parsed.
         if self.threshold is None:
-            if hasattr(self.o, 'hkMemoryMultiplier'):
-                self.o.hkMemoryMultiplier = float(self.o.hkMemoryMultiplier)
-            else:
-                self.o.hkMemoryMultiplier = 3
+            # If the config set something, use it. 
+            if self.o.MemoryMax != -1024:
+                self.threshold = self.o.MemoryMax
 
-            if hasattr(self.o, 'hkMemoryMax'):
-                self.threshold = chunksize_from_str(self.o.hkMemoryMax)
+            if self.threshold is None:
+                # No user input set, now to figure out what our baseline memory usage is at a steady state
+                #   Process at least MemoryBaseLineFile(s) then get a memory reading before actually setting our memory threshold to trigger restart. 
+                if (self.transferCount < self.o.MemoryBaseLineFile) and (self.msgCount < self.o.MemoryBaseLineFile):
+                    # Not enough files processed for steady state, continue to wait..
+                    logger.info(f"Current mem usage: {humanize.naturalsize(mem, binary=True)}, accumulating count ({self.transferCount} or {self.msgCount}/{self.o.MemoryBaseLineFile} so far) before self-setting threshold")
+                    return True
 
-            if (self.o.publish_count < self.file_count) and (
-                    self.o.message_count < self.file_count):
-                logger.info("Current mem usage: %s, accumulating count (%d or %d / %d so far) before setting threshold"
-                            % (humanize.naturalsize(mem, binary=True),
-                                self.o.publish_count,
-                                self.o.message_count,
-                                self.file_count))
-                return True
+                self.threshold = int(self.o.MemoryMultiplier * mem)
 
-        if self.threshold is None:
-            self.threshold = int(self.o.hkMemoryMultiplier * mem)
-            logger.info("hb_memory threshold defaulted to %s" % humanize.naturalsize(self.threshold, binary=True))
+            logger.info(f"Memory threshold set to: {humanize.naturalsize(self.threshold, binary=True)}")
 
-        logger.info("hb_memory, current usage: %s trigger restart if increases past: %s " %
-                    (humanize.naturalsize(mem, binary=True), humanize.naturalsize(self.threshold, binary=True)))
+        logger.info(f"Current Memory usage: {humanize.naturalsize(mem, binary=True)} / {humanize.naturalsize(self.threshold, binary=True)} = {(mem/self.threshold):.2%}")
 
         if mem > self.threshold:
-            self.restart(self)
+            self.restart()
+        # self.restart()
 
         return True
 
     def restart(self):
         """
         """
-        cmd = []
-        # TODO: Is this looking for "sarra"/"sr"/"src"/"sr3" ??
-        #  May be wanting to use sys.argv[0]
-        cmd.append(self.o.program_name)
+        # Do an in-place restart (keeps pid)
+        logger.info(f"Memory threshold surpassed! Triggering a restart for '{sys.executable}' with '{sys.argv}'")
+        # FIXME Verify this actually works in Windows...
+        os.execl(sys.executable, sys.executable, * sys.argv)
 
-        # TODO: Need to figure out where to access these vars (user_{args,config})
-        #  May be wanting to use  sys.argv[1:-1]:
-        if self.o.user_args and len(self.o.user_args) > 0:
-            cmd.extend(self.o.user_args)
-        if self.o.user_config:
-            cmd.append(self.o.user_config)
+        # cmd_list = []
 
-        cmd.append('restart')
+        # program_name == poll|post|sarra|sender|shovel|subscribe|watch
+        # cmd_list.append(self.o.program_name)
 
-        logger.info("hb_memory triggering %s" % cmd)
-        sr_GlobalState.run_command(cmd)
+        # We essentially want to run : sr3 <options> restart <config>.. I think?
+        # cmd_list.append('sr3')
+        # sys.argv == ['/.../sr3/sarracenia/instance.py', '--no', '1', 'start|restart|stop', 'poll|post|sarra|sender|shovel|subscribe|watch/<configFileName>']
+        # cmd_list.extend(sys.argv[1:])
+        # cmd_list[cmd_list.index('start')] = 'restart'
 
-        return
+        # logger.info(f"Memory threshold surpassed! Triggering a restart with '{cmd_list}'")
+        # sr_GlobalState.run_command(self, cmd_list)
 
-# FIXME: A list of things and stuff..
-# - Confirm that the options are actually renamed
-# - Add the entry point in sarracenia/config.py ?somewhere?
-# - Figure out where restart can get args ($*) from.. (sarracenia.config.Config)
-# - Actually run the plugin and see what explodes...
+        # Do an in-place restart (keeps pid)
+
+    def after_work(self, worklist):
+        self.transferCount += len(worklist.ok)
+        # if self.threshold is not None:
+        #    TODO: Remove this callback when isue #444 is implemented
+
+    def after_accept(self, worklist):
+        self.msgCount += len(worklist.incoming)
+        # if self.threshold is not None:
+        #    TODO: Remove this callback when isue #444 is implemented
