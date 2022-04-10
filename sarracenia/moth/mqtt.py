MetPX/sarracenia

commit 95b48fbc4a27b336f9925ffea8773a4a0231534b

more fixes. #492

sarracenia/moth/mqtt.py
@@ -115,7 +115,7 @@ class MQTT(Moth):
         """
 
         super().__init__(broker, options, is_subscriber)
-
+        self.o = options
         self.o.update(default_options)
         self.o.update(options)
 
@@ -379,7 +379,7 @@ class MQTT(Moth):
             logger.info('cleanup sessions for instances')
             for i in range(1,self.o['instances']+1):
                 icid= self.o['queueName'] + '%02d' % i 
-                myclient = self.__clientSetup( options, icid )
+                myclient = self.__clientSetup( self.o, icid )
                 myclient.connect( self.broker.url.hostname, port=self.__sslClientSetup(), \
                    myclean_start=True, properties=props )
                 while not self.client.is_connected():

sarracenia/flowcb/housekeeping/resources.py
@@ -34,12 +34,10 @@ Returns:
 """
 
 import logging
-from pickle import TRUE
 import humanize
 import os
 import psutil
 from sarracenia.flowcb import FlowCB
-from sarracenia.sr import sr_GlobalState
 import sys
 
 logger = logging.getLogger(__name__)

sarracenia/moth/amqp.py
@@ -411,7 +411,7 @@ class AMQP(Moth):
                         self.metrics['rxGoodCount'] += 1
                     if hasattr(self.o, 'fixed_headers'):
                         for k in self.o.fixed_headers:
-                            m[k] = self.o.fixed_headers[k]
+                            msg[k] = self.o.fixed_headers[k]
 
                     logger.debug("new msg: %s" % msg)
                     return msg

tools/dd-xml-inotify.py
@@ -245,13 +245,13 @@ for spath in SRC:
 
     entries = os.listdir(spath)
     wdd = wm.add_watch(spath, pyinotify.IN_CLOSE_WRITE, rec=True)
-    print "watching = " + spath
+    print( "watching = " + spath)
 
     for d in entries:
         currentDir = spath + '/' + d
         if os.path.isfile(currentDir): continue
         wdd = wm.add_watch(currentDir, pyinotify.IN_CLOSE_WRITE, rec=True)
-        print "watching = " + currentDir
+        print( "watching = " + currentDir)
 
 # start event loop
 asyncore.loop()

commit f24613384227038df6ccdac300918c933386784f

fixing breakage of MQTT from 95b48fbc4a27b336f9925ffea8773a4a0231534b mqtt wasn't working at all because options were getting thwacked.

sarracenia/moth/mqtt.py
@@ -113,7 +113,10 @@ class MQTT(Moth):
         """
 
         super().__init__(broker, options, is_subscriber)
-        self.o = options
+
+        # setting this is wrong and breaks things, was already set in super-class init, doing this here overwites
+        #  interpretation of options done in superclass.
+        #self.o = options
         self.o.update(default_options)
         self.o.update(options)
 