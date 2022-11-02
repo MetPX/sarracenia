8d97e8a3a13683660a73fec0c12744759ab04182
Andre <andreleblanc725@gmail.com>
2022-11-02 19:27:52 +0000

    Parse bulletins working

--- sarracenia/flowcb/am.py
+++ sarracenia/flowcb/am.py
@@ -14,12 +14,16 @@ AM (Alphanumeric Messaging) protocol.
 By: André LeBlanc, Autumn 2022
 """
 
+# TODO: Update method descriptions and file description
+
 import logging, socket, struct, time, sys, os
 import urllib.parse
+import pathlib
 import sarracenia
+import sarracenia.config
 from sarracenia.flowcb import FlowCB
 
-#   default_options = {'download': False, 'logReject': False, 'logFormat': '%(asctime)s [%(levelname)s] %(name)s %(funcName)s %(message)s', 'logLevel': 'info', 'sleep': 0.1, 'vip': None}
+# default_options = {'download': False, 'logReject': False, 'logFormat': '%(asctime)s [%(levelname)s] %(name)s %(funcName)s %(message)s', 'logLevel': 'info', 'sleep': 0.1, 'vip': None}
 logger = logging.getLogger(__name__)
 
 
@@ -39,16 +43,16 @@ class AM(FlowCB):
         self.url = urllib.parse.urlparse(self.o.destination)
 
         # Initialise server variables
-        self.inBuffer = None
+        self.inBuffer = bytes()
         self.limit = 32678
         self.o.add_option('onlySync','flag', False)
         self.o.add_option('patternAM','str','80sII4sIIII20s')
         self.o.add_option('sizeAM', 'count', struct.calcsize(self.o.patternAM))
-        self.o.remoteHost = self.url.netloc.split(':')[0]
-        self.o.port = int(self.url.netloc.split(':')[1])
+        self.host = self.url.netloc.split(':')[0]
+        self.port = int(self.url.netloc.split(':')[1])
         self.childlist = []
         self.o.timeCopy = True
-
+        self.remoteHost = None
 
         # Initialise socket
         ## Create a TCP/IP socket
@@ -70,45 +74,52 @@ class AM(FlowCB):
             connected instance
         """      
 
-        if self.o.remoteHost == 'None':
+        if self.host == 'None':
             raise Exception("No remote host was specified.")
 
-        pid = 1
+        try:
+            # Bind socket to all interfaces and listen
+            self.s.bind((self.host, self.port)) 
+            self.s.listen(1)
+            logger.info("Socket listening on remote host %s and port %d.", self.host, self.port)
+            self.flag = True
+        except socket.error as e:
+                logger.info(f"Parent process bind failed. Retrying. Error: {e.args}")
+                time.sleep(5)
+        
+        child_inst = 2
 
         while True:
             
-            try:
-                if pid != 0:    
-                    # Bind socket to all interfaces and listen
-                    self.s.bind(('', self.o.port)) 
-                    self.s.listen(1)
-                    logger.info("Socket binded to host %s and port %d.", self.o.remoteHost, self.o.port)
-                    self.flag = True
-
                 try:
                     # Accept the connection from socket
-                    logger.info("Trying to accept connection from child process")
-                    conn, self.o.remoteHost = self.s.accept()
+                    logger.info("Trying to accept connection from child process.")
+                    conn, self.remoteHost = self.s.accept()
                     
                     # Parent process stays in the loop searching for other connections. 
                     # Child will proceed accepting or refusing connection.
-                    if self.flag == True:
-                        self.childlist.append(pid)
-                        logger.info(f"{pid}")
-                        pid = os.fork()
-                        self.flag = False
+                    pid = os.fork()
 
                     if pid == 0:
                         # Break from the loop if process is child
                         # TODO: Log to file with correct instance ID
                         # TODO: Write the pid file for the instance, so that sr status | stop | sanity
+                        # FIXME: Is this done??
                         self.s.close()
                         break                    
 
                     elif pid == -1:
                         raise logger.exception("Connection could not fork. Exiting.") 
                     else:
-                        # Stay in loop if process is parent
+                        # Stay in loop if process is parent 
+                        self.childlist.append(pid)
+
+                        pidfilename = sarracenia.config.get_pid_filename(
+                        None, self.o.component, self.o.config, child_inst)
+
+                        with open(pidfilename, 'w') as pfn:
+                            pfn.write('%d' % pid)
+                            
                         conn.close()
                         pass
                    
@@ -116,22 +127,18 @@ class AM(FlowCB):
                     logger.info("Couldn't accept connection. Retrying.")
                     time.sleep(1)
                 
-            except socket.error or OSError:
-                # logger.info("Parent process bind failed. Retrying.")
-                time.sleep(5)
-            
-            """
+                """
             try:
             
                 # Bind socket to all interfaces and listen
-                self.s.bind(('', self.o.port)) 
+                self.s.bind(('', self.port)) 
                 self.s.listen(1)
                 logger.info("Socket bound.")
 
                 try:
                     # Accept the connection from socket
                     logger.info("Trying to accept connection.")
-                    conn, self.o.remoteHost = self.s.accept()
+                    conn, self.remoteHost = self.s.accept()
                     break    
 
                 except TypeError:
@@ -143,15 +150,15 @@ class AM(FlowCB):
                 time.sleep(5)          
             """            
 
-        logger.info("Socket accecpted with IP %s and on port %d", self.o.remoteHost, self.o.port)     
+        logger.info("Connection accepted with IP %s on port %d", self.remoteHost, self.port)     
 
-        self.s.close()
+        return conn                       
 
-        return conn               
 
     def on_start(self):
         self.conn = self.__establishconn__()
     
+
     def on_stop(self):
         # Kill all child processes gathered in list
         for child_pid in self.childlist:
@@ -159,46 +166,44 @@ class AM(FlowCB):
         
         self.conn.close()
 
+
     def AddBuffer(self):
         """
-        Overview:
+        Overview::
             Add buffer data from remote server
         
-        Pseudocde:
+        Pseudocode::
             Receive data from remote server
             if only want to sync buffer:
                 Ignore all error logs
 
-        Return:
+        Return::
             None
         """
 
-        while True:
-            try:
-                # Receive data from socket
-                tmp = self.conn.recv(self.limit)
-                time.sleep(0.1)
+        try:
+            # Receive data from socket
+            tmp = self.conn.recv(self.limit)
 
-                if tmp == '':
-                    if not self.o.onlySync:
-                        logger.exception("Connection was lost")
-                        raise Exception("Connection was lost")
-                
-                logger.info("Message length - %d Bytes, Data received from socket - %s" % (len(tmp),tmp))
-                
-                self.inBuffer = tmp
-                break   
 
-            except socket.error:
-                (type, value, tb) = sys.exc_info()
-
-                logger.warning("Type: %s, Value: %s, [socket.recv(%d)]" % (type, value, self.limit))
-                
+            if tmp == '':
                 if not self.o.onlySync:
                     logger.exception("Connection was lost")
                     raise Exception("Connection was lost")
-                
-                break 
+            
+            logger.info("Message length - %d Bytes, Data received from socket - %s" % (len(tmp),tmp))
+            
+            self.inBuffer = self.inBuffer + tmp
+
+        except socket.error:
+            (type, value, tb) = sys.exc_info()
+
+            logger.warning("Type: %s, Value: %s, [socket.recv(%d)]" % (type, value, self.limit))
+            
+            if not self.o.onlySync:
+                logger.exception("Connection was lost")
+                raise Exception("Connection was lost")
+            
 
     def CheckNextMsgStatus(self):
         """
@@ -214,24 +219,42 @@ class AM(FlowCB):
                 return INCOMPLETE
         """
 
-        self.AddBuffer()
-
         logger.info("Verifying message integrity.")
         
         # Only unpack data if buffer length satisfactory
         if len(self.inBuffer) >= self.o.sizeAM:
+            # TODO: Add variables to options with self.o.add_option?
             (header, src_inet, dst_inet, threads, start, length, firsttime, timestamp, future) = \
                     struct.unpack(self.o.patternAM,self.inBuffer[0:self.o.sizeAM])
         else:
             return 'INCOMPLETE'
 
         length = socket.ntohl(length)
-
+        
         if len(self.inBuffer) >= self.o.sizeAM + length:
             return 'OK'
         else:
-            logger.info("Verification failure. Exiting poll.")
             return 'INCOMPLETE'
+
+
+    def unwrapmsg(self):
+
+        status = self.CheckNextMsgStatus()
+
+        if status == 'OK':
+            (self.header,src_inet,dst_inet,threads,start,length,firsttime,timestamp,future) = \
+                    struct.unpack(self.o.patternAM,self.inBuffer[0:self.o.sizeAM])
+
+            length = socket.ntohl(length)
+
+            bulletin = self.inBuffer[self.o.sizeAM:self.o.sizeAM + length]
+            longlen = self.o.sizeAM + length
+            logger.info("Gather successful.")
+            return (bulletin, longlen)
+        
+        else:
+            return '', 0
+
 
     def gather(self):
         """
@@ -247,34 +270,39 @@ class AM(FlowCB):
             else
                 return []
         """
+        self.AddBuffer()
 
-        status = self.CheckNextMsgStatus()
-        
-        # Unpack data
-        if status == 'OK':
-            (header,src_inet,dst_inet,threads,start,length,firsttime,timestamp,future) = \
-                     struct.unpack(self.o.patternAM,self.inBuffer[0:self.o.sizeAM])
+        newmsg = []
 
-            length = socket.ntohl(length)
+        while True:
+            status = self.CheckNextMsgStatus()
+            
+            if status == 'INCOMPLETE':
+                break
 
-            msg = self.inBuffer[self.o.sizeAM:self.o.sizeAM + length]
-            logger.info("Gather successful.")
+            if status == 'OK':
+                ## TODO: Add corrupt data verifier?
+                (bulletin, longlen) = self.unwrapmsg()
 
-            newmsg = []
+                self.inBuffer = self.inBuffer[longlen:]
 
-            # Create a file for new messages and let sarracenia format data
-            filepath = self.o.directory + os.sep + header.split(b'\0',1)[0].decode('iso-8859-1').replace(' ', '_') 
-            file = open(filepath, 'wb')
-            file.write(msg)
-            file.close()
-            st = os.stat(filepath)
+                #Debug
+                logger.info(f"Bulletin contents: {bulletin}")
+                logger.info(f"Bulletin length: {longlen}")       
 
-            sarramsg = sarracenia.Message.fromFileData(filepath,self.o, lstat=st)
-            newmsg.append(sarramsg)
+                # Create a file for new messages and let sarracenia format data
+                # TODO: Change file path to end @ CACN00 CWAO 281900 WRR <-
+                filepath = self.o.directory + os.sep + self.header.split(b'\0',1)[0].decode('iso-8859-1').replace(' ', '_') 
+                file = open(filepath, 'wb')
+                file.write(bulletin)
+                file.close()
+                st = os.stat(filepath)
 
-            return newmsg
-        else:
-            return []                
+                sarramsg = sarracenia.Message.fromFileData(filepath,self.o, lstat=st)
+                newmsg.append(sarramsg)
+
+        return newmsg    
+                                
 
 # Debug
 # if __name__ == '__main__':
