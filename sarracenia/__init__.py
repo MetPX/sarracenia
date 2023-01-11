3faf9119508c0a4dd05beb369fbd40a19052b90d
Peter Silva <peter@bsqt.homeip.net>
2023-01-11 13:42:02 -0500

    switch version away from already released one.

--- sarracenia/__init__.py
+++ sarracenia/__init__.py
@@ -24,7 +24,7 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 #
 #
-__version__ = "3.00.25"
+__version__ = "3.00.26"
 
 from base64 import b64decode, b64encode
 import calendar
