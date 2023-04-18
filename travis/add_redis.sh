55145fca13ba9eb9970443ea425cc2533c3e8f3e
Greg Linton <Greg.Linton@ssc-spc.gc.ca>
2023-04-18 16:21:20 -0400

    Add Redis flow tests

---
+++ travis/add_redis.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+apt-get update
+apt-get install redis
+
+if [[ $(($check_wsl == "init" )) ]]; then
+	sudo service redis-server restart
+else
+	sudo systemctl restart redis-server
+fi
+
+pip3 install redis redis_lock
+
+cat >> ~/.config/sr3/default.conf << EOF
+retry_driver redis
+redisqueue_serverurl redis://localhost:6379/0
+EOF
