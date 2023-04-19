0bd2192d5b6ee9f7ebf8ce21b776c8eea7549065
Greg Linton <Greg.Linton@ssc-spc.gc.ca>
2023-04-18 20:14:18 -0400

    Added vars, and proper python redis locking module

--- travis/add_redis.sh
+++ travis/add_redis.sh
@@ -1,12 +1,14 @@
 sudo apt -y install redis
 
+check_wsl=$(ps --no-headers -o comm 1)
+
 if [[ $(($check_wsl == "init" )) ]]; then
 	sudo service redis-server restart
 else
 	sudo systemctl restart redis-server
 fi
 
-pip3 install redis redis_lock
+pip3 install redis python-redis-lock
 
 cat >> ~/.config/sr3/default.conf << EOF
 retry_driver redis
