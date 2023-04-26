ef7d4f25246ffb482f47686dadc5978d20eec7f1
Greg Linton <Greg.Linton@ssc-spc.gc.ca>
2023-04-26 15:36:19 +0000

    Add user auth to Redis tests

--- travis/add_redis.sh
+++ travis/add_redis.sh
@@ -1,16 +1,25 @@
 sudo apt -y install redis
 
+# enable auth for Redis
+REDIS_SR3_PASSWORD=$(openssl rand -hex 6)
+cat >> /etc/redis/redis.conf << EOF
+user sr3 on >${REDIS_SR3_PASSWORD} ~* +@all
+user default off
+EOF
 check_wsl=$(ps --no-headers -o comm 1)
 
+# Start Redis Service
 if [[ $(($check_wsl == "init" )) ]]; then
 	sudo service redis-server restart
 else
 	sudo systemctl restart redis-server
 fi
 
+# Install Python modules
 pip3 install redis python-redis-lock
 
+# Set redis defaults for sr3
 cat >> ~/.config/sr3/default.conf << EOF
 retry_driver redis
-redisqueue_serverurl redis://localhost:6379/0
+redisqueue_serverurl redis://sr3:${REDIS_SR3_PASSWORD}@localhost:6379/0
 EOF
