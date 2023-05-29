5606a35cad649ea1c068e2bae8f3f62d36973a23
Peter Silva <peter.silva@ssc-spc.gc.ca>
2023-05-29 16:12:18 +0000

    add copy time based on publication date to age.

--- sarracenia/flowcb/work/age.py
+++ sarracenia/flowcb/work/age.py
@@ -23,15 +23,20 @@ class Age(FlowCB):
         self.metrics['ageTotal'] = 0
         self.metrics['ageCount'] = 0
         self.metrics['ageMax'] = 0
+        self.metrics['copyTotal'] = 0
+        self.metrics['copyMax'] = 0
 
     def on_start(self) -> None:
         self.reset_metrics()
 
     def metricsReport(self) -> dict:
+        self.metrics['copyCount'] =  self.metrics['ageCount']
         if self.metrics['ageCount'] > 0:
             self.metrics['ageMean'] = self.metrics['ageTotal']/self.metrics['ageCount']
+            self.metrics['copyMean'] = self.metrics['copyTotal']/self.metrics['ageCount']
         else:
             self.metrics['ageMean'] = 0
+            self.metrics['copyMean'] = 0
 
         return self.metrics
 
@@ -40,6 +45,9 @@ class Age(FlowCB):
         logger.info( "Age of files (in seconds) when transfer complete, maximum: %.2g Average: %.2g file count: %d" % 
             ( self.metrics['ageMax'], self.metrics['ageMean'], self.metrics['ageCount'] ) )
 
+        logger.info( "Copy time for files (in seconds) when transfer complete, maximum: %.2g Average: %.2g file count: %d" % 
+            ( self.metrics['copyMax'], self.metrics['copyMean'], self.metrics['ageCount'] ) )
+
 
     def after_work(self, worklist) -> None:
         for m in worklist.ok:
@@ -47,12 +55,17 @@ class Age(FlowCB):
                 return None
             completed = sarracenia.timestr2flt(m['timeCompleted'])
             mtime = sarracenia.timestr2flt(m['mtime'])
+            pubtime = sarracenia.timestr2flt(m['pubTime'])
             age = completed - mtime
+            copy = completed - pubtime
             self.metrics['ageTotal'] += age
+            self.metrics['copyTotal'] += copy
             self.metrics['ageCount'] += 1
+            if copy > self.metrics['copyMax']:
+                self.metrics['copyMax'] = copy
             if age > self.metrics['ageMax']:
                 self.metrics['ageMax'] = age
 
-            logger.info("file %s is %d seconds old" % (m['new_dir']+os.sep+m['new_file'], age))
+            logger.info( f"file {m['new_dir']+os.sep+m['new_file']} took {copy} seconds to copy and is {age} seconds old"  )
 
      
