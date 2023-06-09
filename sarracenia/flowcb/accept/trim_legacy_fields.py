7942aa475e14e01dbc33a5ecff960923c038519a
Peter Silva <peter@bsqt.homeip.net>
2023-06-09 09:11:20 -0400

    new trim_legacy_fields plugin for simpler messages #615

---
+++ sarracenia/flowcb/accept/trim_legacy_fields.py
@@ -0,0 +1,24 @@
+"""
+Plugin :
+    This plugin is aesthetic... some users want less cluttered messages, remove fields
+    that are not needed in the non-file replication case, and where sundew_compatibility
+    is irrelevant.
+
+Usage:
+    callback accept.trim_legacy_fields
+"""
+
+import logging
+import os, stat, time
+from sarracenia.flowcb import FlowCB
+
+logger = logging.getLogger(__name__)
+
+
+class Trim_legacy_fields(FlowCB):
+
+    def after_accept(self, worklist):
+        for message in worklist.incoming:
+            for h in [ 'atime', 'filename', 'from_cluster', 'mtime', 'source', 'sundew_extension', 'to_clusters' ]:
+                if h in message:
+                    del message[h]
