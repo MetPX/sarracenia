3540c337adcc08273721c7fa66051ee70f756286
Greg Linton <Greg.Linton@ssc-spc.gc.ca>
2023-07-28 00:13:53 -0400

    Bunch more callback tests in flowcb/accept

---
+++ tests/sarracenia/flowcb/accept/tohttp_test.py
@@ -0,0 +1,68 @@
+import pytest
+import types
+
+#useful for debugging tests
+def pretty(*things, **named_things):
+    import pprint
+    for t in things:
+        pprint.PrettyPrinter(indent=2, width=200).pprint(t)
+    for k,v in named_things.items():
+        print(str(k) + ":")
+        pprint.PrettyPrinter(indent=2, width=200).pprint(v)
+
+from sarracenia.flowcb.accept.tohttp import ToHttp
+from sarracenia import Message as SR3Message
+import sarracenia.config
+
+def make_message(isRetry = False):
+    m = SR3Message()
+    m['baseUrl'] = 'http://NotAReal.url'
+    m['relPath'] = 'a/rel/Path/file.txt'
+    m['pubTime'] = '20180118151049.356378078'
+    m['urlstr'] = 'file://fake/path'
+    m['savedurl'] = '/new/fake/path'
+
+    return m
+
+def make_worklist():
+    WorkList = types.SimpleNamespace()
+    WorkList.ok = []
+    WorkList.incoming = []
+    WorkList.rejected = []
+    WorkList.failed = []
+    WorkList.directories_ok = []
+    return WorkList
+
+
+def test_after_accept():
+    #Set x - When random is True, with a message having isRetry
+    options = sarracenia.config.default_config()
+    worklist = make_worklist()
+    worklist.incoming = [make_message()]
+    with pytest.raises(TypeError):
+        tohttp = ToHttp(options)
+
+    #Set x - When random is True, with a message having isRetry
+    options = sarracenia.config.default_config()
+    options.baseDir = "/fake/path"
+    worklist = make_worklist()
+    worklist.incoming = [make_message()]
+    tohttp = ToHttp(options)
+    tohttp.after_accept(worklist)
+    assert len(worklist.incoming) == 1
+    assert worklist.incoming[0]['urlstr'] == '/new/fake/path'
+    assert worklist.incoming[0]['set_notice'] == '%s %s %s' % (worklist.incoming[0]['pubTime'], 'file:', worklist.incoming[0]['relPath'])
+
+    #Set x - When random is True, with a message having isRetry
+    options = sarracenia.config.default_config()
+    options.toHttpRoot = ["/fake/path"]
+    worklist = make_worklist()
+    worklist.incoming = [make_message()]
+    tohttp = ToHttp(options)
+    tohttp.after_accept(worklist)
+    assert len(worklist.incoming) == 1
+    assert worklist.incoming[0]['urlstr'] == '/new/fake/path'
+    assert worklist.incoming[0]['set_notice'] == '%s %s %s' % (worklist.incoming[0]['pubTime'], 'file:', worklist.incoming[0]['relPath'])
+
+
+
