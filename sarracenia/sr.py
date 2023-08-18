1fea06f1f8fba60bfaaa7b72851e1e4737deebd9
Peter Silva <peter@bsqt.homeip.net>
2023-08-18 01:30:18 -0400

    one last stray comma on a line by itself.

--- sarracenia/sr.py
+++ sarracenia/sr.py
@@ -2147,7 +2147,7 @@ class sr_GlobalState:
         print('\n\n"Processes" : { \n\n')
         procs_length = len(self.procs)
         for index,pid in enumerate(self.procs):
-            print('\t\"%s\": %s' % (pid, json.dumps(self.procs[pid], sort_keys=True, indent=4) ))
+            print('\t\"%s\": %s' % (pid, json.dumps(self.procs[pid], sort_keys=True, indent=4)), end='')
             if procs_length-1 > index:
                 print(',')
         print('},') 
