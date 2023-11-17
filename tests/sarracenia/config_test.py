a7edfdb25ba85e029971e213f1432a8ec8c4ae3c
Peter Silva <peter@bsqt.homeip.net>
2023-11-17 13:33:50 -0500

    adding tests (that fail without fixes here) for #833

--- tests/sarracenia/config_test.py
+++ tests/sarracenia/config_test.py
@@ -18,6 +18,16 @@ pretty = pprint.PrettyPrinter(indent=2, width=200).pprint
 logger = logging.getLogger('sarracenia.config')
 logger.setLevel('DEBUG')
 
+def try_pattern( options, message, pattern, goodre ):
+    """
+       given a pattern, call variable expansion, and confirm it matches the resulting regex.
+
+    """
+    result = options.variableExpansion( pattern, message );
+    logger.debug( f"input: {pattern} result: {result}" )
+    good = re.compile( goodre )
+    return good.match(result)
+
 def test_variableExpansion():
 
      options = sarracenia.config.default_config()
@@ -30,30 +40,16 @@ def test_variableExpansion():
      message['source'] = 'WhereDataComesFrom'
      message['baseUrl'] = 'https://localhost/smorgasbord'
 
-     result = options.variableExpansion( "${PDR}/${%Y%m%d}/", message );
-     logger.debug( f"result: {result}" )
-     good = re.compile( options.post_baseDir + '/[0-9]{8}/' )
-     assert good.match(result)
+     assert try_pattern(  options, message, "${PDR}/${%Y%m%d}/",  options.post_baseDir + '/[0-9]{8}/' )
+     assert try_pattern(  options, message, "${PBD}/${%Y%m%d}/",  options.post_baseDir + '/[0-9]{8}/' )
+     assert try_pattern(  options, message, "${PBD}/${SOURCE}/${%Y%m%d}/boughsOfHolly",  options.post_baseDir + '/' + message['source'] + '/[0-9]{8}/boughsOfHolly' )
+     assert try_pattern(  options, message, "${PBD}/${BUPL}/${%Y%m%d}/falala",  options.post_baseDir + '/smorgasbord/[0-9]{8}/falala' )
+     assert try_pattern(  options, message, "${PBD}/${BUPL}/${%Y%m%d}/",  options.post_baseDir + '/smorgasbord/[0-9]{8}/' )
+     assert try_pattern(  options, message, "${PBD}/${%Y%m%d}/${SOURCE}/AIRNOW/CSV/BCMOE/${%H}/",  \
+             options.post_baseDir + '/[0-9]{8}/'+ message['source'] +'/AIRNOW/CSV/BCMOE/[0-9]{2}/' )
+     assert try_pattern(  options, message, "${PBD}/${%Y%m%d}/${SOURCE}/AIRNOW/CSV/BCMOE/${%o-4h%H}/",  \
+             options.post_baseDir + '/[0-9]{8}/'+ message['source'] +'/AIRNOW/CSV/BCMOE/[0-9]{2}/' )
 
-     result = options.variableExpansion( "${PBD}/${%Y%m%d}/", message );
-     logger.debug( f"result: {result}" )
-     good = re.compile( options.post_baseDir + '/[0-9]{8}/' )
-     assert good.match(result)
-
-     result = options.variableExpansion( "${PBD}/${SOURCE}/${%Y%m%d}/boughsOfHolly", message );
-     logger.debug( f"result: {result}" )
-     good = re.compile( options.post_baseDir + '/' + message['source'] + '/[0-9]{8}/boughsOfHolly' )
-     assert good.match(result)
-
-     result = options.variableExpansion( "${PBD}/${BUPL}/${%Y%m%d}/falala", message );
-     logger.debug( f"result: {result}" )
-     good = re.compile( options.post_baseDir + '/smorgasbord/[0-9]{8}/falala' )
-     assert good.match(result)
-
-     result = options.variableExpansion( "${PBD}/${BUPL}/${%Y%m%d}/", message );
-     logger.debug( f"result: {result}" )
-     good = re.compile( options.post_baseDir + '/smorgasbord/[0-9]{8}/' )
-     assert good.match(result)
 
      # to get stuff to print out, make it fail.
      #assert False 
