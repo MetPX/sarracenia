c92b026d97924831f8a4ecae5487b63108f7f6d0
Andre LeBlanc <andre.leblanc@ssc-spc.gc.ca>
2024-03-25 15:07:51 +0000

    #997 Move retry logic to on_start

--- sarracenia/flowcb/retry.py
+++ sarracenia/flowcb/retry.py
@@ -61,17 +61,6 @@ class Retry(FlowCB):
 
         #queuedriver = os.getenv('SR3_QUEUEDRIVER', 'disk')
 
-        if self.o.retry_driver == 'redis':
-            from sarracenia.redisqueue import RedisQueue
-            self.download_retry = RedisQueue(options, 'work_retry')
-            self.post_retry = RedisQueue(options, 'post_retry')
-        else:
-            from sarracenia.diskqueue import DiskQueue
-            self.download_retry_name = 'work_retry_%02d' % options.no
-            self.download_retry = DiskQueue(options, self.download_retry_name)
-            self.post_retry_name = 'post_retry_%03d' % options.no
-            self.post_retry = DiskQueue(options, self.post_retry_name)
-
         logger.debug('logLevel=%s' % self.o.logLevel)
 
 
@@ -175,6 +164,19 @@ class Retry(FlowCB):
 
         self.download_retry.on_housekeeping()
         self.post_retry.on_housekeeping()
+
+    def on_start(self) -> None:
+
+        if self.o.retry_driver == 'redis':
+            from sarracenia.redisqueue import RedisQueue
+            self.download_retry = RedisQueue(self.o, 'work_retry')
+            self.post_retry = RedisQueue(self.o, 'post_retry')
+        else:
+            from sarracenia.diskqueue import DiskQueue
+            self.download_retry_name = 'work_retry_%02d' % self.o.no
+            self.download_retry = DiskQueue(self.o, self.download_retry_name)
+            self.post_retry_name = 'post_retry_%03d' % self.o.no
+            self.post_retry = DiskQueue(self.o, self.post_retry_name)
 
     def on_stop(self) -> None:
         self.download_retry.close()
