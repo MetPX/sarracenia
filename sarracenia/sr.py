019b16ac33a75549d904cc20264640d8e368455f
Peter Silva <Peter.Silva@ssc-spc.gc.ca>
2024-04-22 18:28:00 +0000

    adding #1022 sftp automated conversion

--- sarracenia/sr.py
+++ sarracenia/sr.py
@@ -2655,11 +2655,12 @@ class sr_GlobalState:
         pos_args_present=False
         with open(v3_config_path, 'w') as v3_cfg:
             v3_cfg.write( f'# created by: sr3 convert {cfg}\n')
-
             if component in [ 'shovel', 'winnow' ]:
                 v3_cfg.write('# topicCopy on is only there for bug-for-bug compat with v2. turn it off if you can.\n')
-                v3_cfg.write('topicCopy on\n')
+                v3_cfg.write('#topicCopy on\n')
 
+            v3_cfg.write('#v2 sftp handling is always absolute, sr3 is relative. This plugin helps during conversion, remove when all sr3:')
+            v3_cfg.write('#flowcb accept.sftp_absolute')
             with open(v2_config_path, 'r') as v2_cfg:
                 for line in v2_cfg.readlines():
                     if len(line.strip()) < 1:
