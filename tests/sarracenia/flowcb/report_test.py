1d744e1127b1544fee520579d9f4c57fb456a72d
Greg Linton <Greg.Linton@ssc-spc.gc.ca>
2024-05-16 11:51:36 -0400

    Add flowcb test files

---
+++ tests/sarracenia/flowcb/report_test.py
@@ -0,0 +1,13 @@
+import pytest
+#from unittest.mock import Mock
+
+import os
+import logging
+
+import sarracenia.config
+import sarracenia.flowcb.report
+
+#useful for debugging tests
+import pprint
+pretty = pprint.PrettyPrinter(indent=2, width=200).pprint
+
