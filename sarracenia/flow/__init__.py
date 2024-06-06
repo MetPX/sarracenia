f2adcbb58e7a99b5fad71a6bac68e7bb529bffe0
Peter Silva <peter@bsqt.homeip.net>
2024-06-05 23:35:13 -0400

    fix #1089, implementing fileAgeMax support in mainline

--- sarracenia/flow/__init__.py
+++ sarracenia/flow/__init__.py
@@ -962,8 +962,13 @@ class Flow:
             if self.o.messageAgeMax != 0 and lag > self.o.messageAgeMax:
                 self.reject(
                     m, 504,
-                    "Excessive lag: %g sec. Skipping download of: %s, " %
-                    (lag, m['new_file']))
+                    f"message too old (high lag): {lag:g} sec. skipping: {m['new_file']}, " )
+                continue
+
+            if self.o.fileAgeMax > 0 and 'mtime' in m:
+                age =  now-sarracenia.timestr2flt(m['mtime'])
+                if age > self.o.fileAgeMax:
+                    self.reject( m, 410, f"file too old: {age:g} sec. skipping: {m['new_file']}, ")
                 continue
 
             if 'fileOp' in m and 'rename' in m['fileOp']:
