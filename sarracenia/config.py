b2dbb37f706e2126beb1641f27e5f62b5be97a3b
Peter Silva <peter.silva@ssc-spc.gc.ca>
2024-06-07 13:28:37 -0400

    reinstating float type options

--- sarracenia/config.py
+++ sarracenia/config.py
@@ -135,8 +135,8 @@ default_options = {
 
 count_options = [
     'batch', 'count', 'exchangeSplit', 'instances', 'logRotateCount', 'no', 
-    'post_exchangeSplit', 'prefetch', 'messageCountMax', 'messageRateMax', 
-    'messageRateMin', 'runStateThreshold_reject', 'runStateThreshold_retry', 'runStateThreshold_slow'
+    'post_exchangeSplit', 'prefetch', 'messageCountMax', 
+    'runStateThreshold_reject', 'runStateThreshold_retry', 'runStateThreshold_slow'
 ]
 
 
@@ -151,7 +151,7 @@ flag_options = [ 'acceptSizeWrong', 'acceptUnmatched', 'amqp_consumer', 'baseUrl
     'statehost', 'users', 'v2compatRenameDoublePost', 'wololo'
                 ]
 
-float_options = [ ]
+float_options = [ 'messageRateMax', 'messageRateMin' ]
 
 duration_options = [
     'expire', 'housekeeping', 'logRotateInterval', 'message_ttl', 'fileAgeMax', 'fileAgeMin', 'metrics_writeInterval', \
@@ -343,6 +343,23 @@ def parse_count(cstr):
         offset=0
     count=humanfriendly.parse_size(cstr[offset:], binary=cstr[-1].lower() in ['i','b'] )
     return -count if offset else count
+
+def parse_float(cstr):
+    if type(cstr) is not str:
+        return cstr
+
+    fa = parse_count(cstr)
+    if abs(fa) < 1000:
+        if cstr[-1] in [ 'k', 'b', 'i' ]:
+            if cstr[-1] in [ 'b', 'i' ]:
+                fa=float(cstr[0:-2])*1024
+            else:
+                if cstr[-2] != 'k':
+                    logger.error( f"malformed float: {cstr}, overriding to kilo" )
+                fa=float(cstr[0:-1])*1000
+        else:
+            fa=float(cstr)
+    return fa
 
 def get_package_lib_dir():
     return os.path.dirname(inspect.getfile(Config))
@@ -1113,7 +1130,7 @@ class Config:
         elif kind == 'float' or kind == float :
             float_options.append(option)
             if type(v) is not float:
-                setattr(self, option, float(v))
+                setattr(self, option, parse_float(v))
         elif kind == 'list' or kind == list:  
             list_options.append( option )
             if type(v) is not list:
@@ -1622,7 +1639,7 @@ class Config:
             setattr(self, k, durationToSeconds(v))
         elif k in float_options:
             try:
-                setattr(self, k, float(v))
+                setattr(self, k, parse_float(v))
             except (ValueError, TypeError) as e:
                 logger.error(f"{','.join(self.files)}:{self.lineno} Ignored '{i}': {e}")
         elif k in perm_options:
@@ -1672,7 +1689,7 @@ class Config:
             v = ' '.join(line[1:])
             if hasattr(self, k):
                 if type(getattr(self, k)) is float:
-                    setattr(self, k, float(v))
+                    setattr(self, k, parse_float(v))
                 elif type(getattr(self, k)) is int:
                     # the only integers that have units are durations.
                     # integers without units will come out unchanged.
@@ -1854,7 +1871,7 @@ class Config:
 
         for f in float_options:
             if hasattr(self, f) and (type(getattr(self, f)) is str):
-                setattr(self, f, float(getattr(self, f)))
+                setattr(self, f, parse_float(getattr(self, f)))
 
         if ( (len(self.logEvents) > 0 ) or self.log_flowcb_needed) :
             if ('sarracenia.flowcb.log.Log' not in self.plugins_late) and \
@@ -2027,7 +2044,7 @@ class Config:
                     setattr(self,u,isTrue(getattr(self,u)))
             elif u in float_options:
                 if type( getattr(self,u) ) is not float:
-                    setattr(self,u,float(getattr(self,u)))
+                    setattr(self,u,parse_float(getattr(self,u)))
             elif u in set_options:
                 if type( getattr(self,u) ) is not set:
                     setattr(self,u,self._parse_set_string(getattr(self,u),set()))
