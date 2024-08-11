6fa84a7735f67015d528a2048a40d1109f4e5af3
Peter Silva <peter@bsqt.homeip.net>
2024-08-10 21:23:22 -0400

    remove far too common uninformative info message

--- sarracenia/flow/__init__.py
+++ sarracenia/flow/__init__.py
@@ -235,7 +235,7 @@ class Flow:
         self.metrics=self.new_metrics
 
         # removing old metrics files
-        logger.info( f"looking for old metrics for {self.o.metricsFilename}" )
+        #logger.debug( f"looking for old metrics for {self.o.metricsFilename}" )
         old_metrics=sorted(glob.glob(self.o.metricsFilename+'.*'))[0:-self.o.logRotateCount]
         for o in old_metrics:
             logger.info( f"removing old metrics file: {o} " )
