59852f0ab6dc50687dd9db18da6e19cbbc10d1b4
Peter Silva <peter@bsqt.homeip.net>
2024-08-17 16:59:22 -0400

    added subscriptions, though not yet used

--- sarracenia/config/subscription.py
+++ sarracenia/config/subscription.py
@@ -1,4 +1,10 @@
 
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 class Subscription(dict):
 
     def __init__(self, options, queueName, subtopic):
@@ -6,7 +12,44 @@ class Subscription(dict):
         self['broker'] = options.broker
         self['topic'] = { 'exchange': options.exchange, 'prefix': options.topicPrefix, 'sub': subtopic }
         self['queue']={ 'name': queueName }
-        for a in [ 'auto_delete', 'broker', 'durable', 'exchange', 'expire', \
+        for a in [ 'auto_delete', 'durable', 'exchange', 'expire', \
                 'message_ttl', 'prefetch', 'qos', 'queueBind', 'queueDeclare', 'topicPrefix' ]:
             if hasattr(options, a) and getattr(options,a):
                 self['queue'][a] = getattr(options,a)
+
+
+class Subscriptions(list):
+    # list of subscription
+
+    def read(self,options,fn):
+        try:
+            with open(fn,'r') as f:
+                #self=json.loads(f.readlines())
+                self=json.load(f)
+
+            for s in self:
+                if type(s['broker']) is str:
+                    ok, broker = options.credentials.validate_urlstr(s['broker'])
+                    if ok:
+                        s['broker'] = broker
+        except Exception as Ex:
+            logger.error( f"failed to read to {fn}: {Ex}" )
+            logger.debug('Exception details: ', exc_info=True)
+        return self
+
+    def write(self,fn):
+
+        jl=[]
+        for s in self:
+            jd=s
+            jd['broker']=str(s['broker'])
+            jl.append(jd)
+
+        try:
+            with open(fn,'w') as f:
+                f.write(json.dumps(jl))
+        except Exception as Ex:
+            logger.error( f"failed to write to {fn}: {Ex}" )
+            logger.debug('Exception details: ', exc_info=True)
+
+     
