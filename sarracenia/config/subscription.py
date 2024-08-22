7eeb498d87833b054d67a32973b7985579da235b
Peter Silva <peter@bsqt.homeip.net>
2024-08-22 09:19:01 -0400

    starting unit tests for subscriptions

--- sarracenia/config/subscription.py
+++ sarracenia/config/subscription.py
@@ -10,12 +10,12 @@ class Subscription(dict):
     def __init__(self, options, queueName, subtopic):
 
         self['broker'] = options.broker
-        self['topic'] = { 'exchange': options.exchange, 'prefix': options.topicPrefix, 'sub': subtopic }
+        self['bindings'] = [ { 'exchange': options.exchange, 'prefix': options.topicPrefix, 'sub': subtopic } ]
         self['queue']={ 'name': queueName }
-        for a in [ 'auto_delete', 'durable', 'exchange', 'expire', \
-                'message_ttl', 'prefetch', 'qos', 'queueBind', 'queueDeclare', 'topicPrefix' ]:
+        for a in [ 'auto_delete', 'durable', 'expire', 'message_ttl', 'prefetch', 'qos', 'queueBind', 'queueDeclare' ]:
+            aa = a.replace('queue','').lower()
             if hasattr(options, a) and getattr(options,a):
-                self['queue'][a] = getattr(options,a)
+                self['queue'][aa] = getattr(options,a)
 
 
 class Subscriptions(list):
@@ -32,10 +32,11 @@ class Subscriptions(list):
                     ok, broker = options.credentials.validate_urlstr(s['broker'])
                     if ok:
                         s['broker'] = broker
+            return self
         except Exception as Ex:
             logger.error( f"failed to read to {fn}: {Ex}" )
             logger.debug('Exception details: ', exc_info=True)
-        return self
+            return None
 
     def write(self,fn):
 
@@ -52,4 +53,31 @@ class Subscriptions(list):
             logger.error( f"failed to write to {fn}: {Ex}" )
             logger.debug('Exception details: ', exc_info=True)
 
-     
+    def add(self, new_subscription):
+
+        found=False
+        for s in self:
+            if ( s['broker'] == new_subscription['broker'] ) and \
+               ( s['queue']['name'] == new_subscription['queue']['name'] ):
+               for b in s['bindings']:
+                   newb = new_subscription['bindings'][0]
+                   if (b['sub'] != newb['sub']) or (b['prefix'] != newb['prefix']):
+                      s['bindings'].append( { 'exchange': newb['exchange'], \
+                                   'prefix':newb['prefix'], 'sub':newb['sub'] } )
+                      found=True
+
+        if not found:
+            self.append(new_subscription)
+
+            
+    def differences(self, other):
+        """
+           return None if there are no differentces.
+        """
+        if self == other:
+            return None
+
+        different_subscriptons=[]
+        
+
+
