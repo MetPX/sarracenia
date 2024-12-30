d0b15a4df2652b8396d6dc8ad16a4b901bc4cefe
Andre LeBlanc <andre.leblanc@ssc-spc.gc.ca>
2024-12-30 20:28:03 +0000

    Add logic from gitlab version of plugins

--- sarracenia/flowcb/poll/airnow.py
+++ sarracenia/flowcb/poll/airnow.py
@@ -30,35 +30,39 @@ class Airnow(FlowCB):
         sleep = self.o.scheduled_interval
 
         gathered_messages = []
-        for Hours in range(1, 3):
-            last_hour_date_time = datetime.datetime.now() - datetime.timedelta(
-                hours=Hours)
-            Filename = 'HourlyData_%s.dat' % last_hour_date_time.strftime(
-                '%Y%m%d%H')
-            logger.debug("poll_airnow_http Filename: %s" % Filename)
-            URL = self.o.pollUrl + '/' + Filename
-            logger.info('INFO %s ' % URL)
-            #resp = requests.get(self.o.pollUrl + '/' + Filename)
-            resp = requests.get(URL)
-            if resp.ok:
-                mtime = datetime.datetime.strptime(resp.headers['last-modified'],\
-                     '%a, %d %b %Y %H:%M:%S %Z')
-                last_poll = datetime.datetime.utcnow() + datetime.timedelta(
-                    seconds=-sleep)
-                logger.info(mtime)
-                logger.info(last_poll)
+        try:
+            for Hours in range(1, 3):
+                last_hour_date_time = datetime.datetime.now() - datetime.timedelta(
+                    hours=Hours)
+                Filename = 'HourlyData_%s.dat' % last_hour_date_time.strftime(
+                    '%Y%m%d%H')
+                logger.debug("poll_airnow_http Filename: %s" % Filename)
+                URL = self.o.pollUrl + '/' + Filename
+                logger.info('INFO %s ' % URL)
+                #resp = requests.get(self.o.pollUrl + '/' + Filename)
+                resp = requests.get(URL)
+                if resp.ok:
+                    mtime = datetime.datetime.strptime(resp.headers['last-modified'],\
+                        '%a, %d %b %Y %H:%M:%S %Z')
+                    last_poll = datetime.datetime.utcnow() + datetime.timedelta(
+                        seconds=-sleep)
+                    logger.info(mtime)
+                    logger.info(last_poll)
 
-                fakeStat = paramiko.SFTPAttributes()
-                fakeStat.st_size = int(resp.headers['content-length'])
+                    fakeStat = paramiko.SFTPAttributes()
+                    fakeStat.st_size = int(resp.headers['content-length'])
 
-                # convert datetime to numeric timestamp from beginning of POSIX epoch.
-                fakeStat.st_mtime = mtime.timestamp()
-                fakeStat.st_atime = mtime.timestamp()
-                fakeStat.st_mode = 0o644
+                    # convert datetime to numeric timestamp from beginning of POSIX epoch.
+                    fakeStat.st_mtime = mtime.timestamp()
+                    fakeStat.st_atime = mtime.timestamp()
+                    fakeStat.st_mode = 0o644
 
-                m = sarracenia.Message.fromFileInfo(Filename, self.o, fakeStat)
-                gathered_messages.append(m)
+                    m = sarracenia.Message.fromFileInfo(Filename, self.o, fakeStat)
+                    gathered_messages.append(m)
 
-                logger.info('mtime: %s  last_pollL %s' % (mtime, last_poll))
-
+                    logger.info('mtime: %s  last_pollL %s' % (mtime, last_poll))
+        except Exception as e:
+            logger.error(f"Poll failed: {e}")
+            logger.debug("Exception details:", exc_info=True)
+        
         return gathered_messages
