e930427ab2bb6093179fe9eaa77bc9ca31db10fa
Reid Sunderland <reid.sunderland@ssc-spc.gc.ca>
2025-02-20 21:20:37 +0000

    nodupe tweaks, still needs more thorough documentation

---
+++ sarracenia/flowcb/nodupe/path_only.py
@@ -0,0 +1,18 @@
+import logging
+from sarracenia.flowcb import FlowCB
+
+logger = logging.getLogger(__name__)
+
+
+class Path_only(FlowCB):
+    """
+      Compared to path, path_only also ignores the checksum, size, modification time, etc. so that
+      all messages with the same relPath are considered duplicates.
+    """
+    def after_accept(self, worklist):
+        for m in worklist.incoming:
+            if not 'nodupe_override' in m:
+                m['_deleteOnPost'] |= set(['nodupe_override'])
+                m['nodupe_override'] = {}
+
+            m['nodupe_override']['key'] = m['relPath']
