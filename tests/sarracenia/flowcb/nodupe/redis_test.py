98927830f0ccde0f67866384ce7ef02860f3e955
Peter Silva <peter@bsqt.homeip.net>
2025-02-24 23:34:08 -0500

    Correcting messages in unit tests so they are closer to ok.

--- tests/sarracenia/flowcb/nodupe/redis_test.py
+++ tests/sarracenia/flowcb/nodupe/redis_test.py
@@ -31,18 +31,15 @@ def make_message():
     m["pubTime"] = "20180118151049.356378078"
     m["topic"] = "v02.post.sent_by_tsource2send"
     m["mtime"] = "20180118151048"
-    m["headers"] = {
-            "atime": "20180118151049.356378078", 
-            "from_cluster": "localhost",
-            "mode": "644",
-            "parts": "1,69,1,0,0",
-            "source": "tsource",
-            "sum": "d,c35f14e247931c3185d5dc69c5cd543e",
-            "to_clusters": "localhost"
+    m["identity"] = {  
+                "method" : "sha512", 
+                "value" : "k5z2h7QHH2ZCC9x0YX2aZa+fC4CgTlFp3I0lntR94ZqbLOhqDHeJWnIi0+mym9pg9e8rS4N9v3IWQm\nXMIdF7CQ=="  
         }
+    m["atime"] = "20180118151049.356378078"
+    m["mode"] = "644"
+    m["size"] = 69
     m["baseUrl"] =  "https://NotARealURL"
     m["relPath"] = "ThisIsAPath/To/A/File.txt"
-    m["notice"] = "20180118151050.45 ftp://anonymous@localhost:2121 /sent_by_tsource2send/SXAK50_KWAL_181510___58785"
     m["_deleteOnPost"] = set()
     return m
 
