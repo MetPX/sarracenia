c1ff7e27e8429fe7b27a5b47df8858284d83b277
Peter Silva <peter@bsqt.homeip.net>
2025-10-03 02:25:21 -0400

    for #1503, Looking at the case reported, detect a path that is a directory op that ends in / then remove the slash (for just that case.) This is the narrowest way to address it.

--- sarracenia/flow/__init__.py
+++ sarracenia/flow/__init__.py
@@ -1950,8 +1950,13 @@ class Flow:
                 continue
 
             if not 'new_file' in msg or not msg['new_file']:
-                self.reject(msg, 422, f"new_file message field missing, do not know name of file to write. skipping." )
-                continue
+                # issue #1503 move a little to the right to deal with when directory path ends in /
+                if len(msg['new_dir']) < 2 or ('fileOp' not in msg and 'directory' in msg['fileOp']): 
+                    self.reject(msg, 422, f"new_file message field missing, do not know name of file to write. skipping." )
+                    continue
+                np=os.path.dirname(msg['new_dir'])
+                msg['new_dir'] = os.path.dirname(np)
+                msg['new_file'] = os.path.basename(np)
 
             new_path = msg['new_dir'] + os.path.sep + msg['new_file']
             new_file = msg['new_file']
