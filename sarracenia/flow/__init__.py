ee93e62687179caf40dc830b44201dfd205289ee
André LeBlanc <51410564+andreleblanc11@users.noreply.github.com>
2025-10-21 11:53:50 -0300

    Issue1495 - Changes in checksum algorithm on download (#1510)
    * Improve checksum evaluation on download
    
    * Fix for #1509
    
    * More improvements to checksum download algorithm to get desired results.
    
    * Fix unit tests as well

--- sarracenia/flow/__init__.py
+++ sarracenia/flow/__init__.py
@@ -2309,13 +2309,19 @@ class Flow:
             # FIXME  locking for i parts in temporary file ... should stay lock
             # and file_reassemble... take into account the locking
 
-            if self.o.identity_method.startswith('cod,'):
-                download_algo = self.o.identity_method[4:]
-            elif 'identity' in msg:
+            # First try to fetch identity from the incoming message
+            if 'identity' in msg:
                 if msg['identity']['method'] == 'cod':
                     download_algo = msg['identity']['value']
+                elif msg['identity']['method'].startswith('cod,'):
+                    download_algo = msg['identity']['method'][4:]
+                # Algo not cod, get value from method field.
                 else:
+                    # We want to re-calculate the checksum to compare with the value that was advertised from the message.
                     download_algo = msg['identity']['method']
+            # Assign whatever is set in the configuration if identity isn't found in the incoming message.
+            elif self.o.identity_method != None:
+                download_algo = self.o.identity_method
             else:
                 download_algo = None
 
@@ -2437,8 +2443,17 @@ class Flow:
                 msg['onfly_checksum'] = self.proto[self.scheme].get_sumstr()
                 msg['data_checksum'] = self.proto[self.scheme].data_checksum
 
-                if self.o.identity_method.startswith('cod,') and not accelerated:
+                if ('identity' in msg and msg['identity']['method'].startswith('cod') or ('identity' not in msg and 'cod' not in self.o.identity_method)) and not accelerated:
+                    # if 'identity' in msg and msg['identity']['value'] != msg['onfly_checksum']['value']:
+                    #     logger.warning("Onfly checksum differs from checksum found in sarracenia message. Will overwrite message with onfly_checksum value.")
+                    #     logger.debug(f"Onfly checksum: {msg['onfly_checksum']} ; Checksum from incoming sarracenia message {msg['identity']}")
                     msg['identity'] = msg['onfly_checksum']
+                # If no incoming checksum in message and have cod in options, add cod checksum to message.
+                elif 'identity' not in msg and 'cod' in self.o.identity_method:
+                    msg['identity'] = {
+                            'method' : 'cod',
+                            'value': download_algo[4:]
+                            }
 
                 msg['_deleteOnPost'] |= set(['onfly_checksum'])
                 msg['_deleteOnPost'] |= set(['data_checksum'])
