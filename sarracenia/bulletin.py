49d155c07336bb72f538555792306ea72605443a
Andre LeBlanc <andre.leblanc@ssc-spc.gc.ca>
2026-03-04 20:08:32 +0000

    Add logic to prevent combined METARs to propogate station name

--- sarracenia/bulletin.py
+++ sarracenia/bulletin.py
@@ -194,8 +194,13 @@ class Bulletin:
 
             #print " ********************* header = ", data[0][0:7]
             # switch depends on bulletin type.
+
             if data[0][0:2] == "SA":
-                if data[1].split()[0] in ["METAR","LWIS"]:
+                # If there are more then 1 METAR in a SA file, don't use the station found as the header.
+                metar_count = sum(s.count('METAR') for s in data)
+                if metar_count > 1:
+                    station = ''
+                elif data[1].split()[0] in ["METAR","LWIS"]:
                     station = premiereLignePleine.split()[1]
                 else:
                     station = premiereLignePleine.split()[0]
