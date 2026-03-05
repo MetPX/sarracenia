11007898764b8aefeae816855e2364cf23b6e148
Reid Sunderland <reid.sunderland@ssc-spc.gc.ca>
2026-03-05 17:09:23 +0000

    fix content inlining from local paths

--- sarracenia/__init__.py
+++ sarracenia/__init__.py
@@ -1140,7 +1140,7 @@ class Message(dict):
             return
 
         try:
-            content = msg.getContent()
+            content = msg.getContent(options)
             sz = len(content)
 
             # We want to update the message size with the recently fetched content.
