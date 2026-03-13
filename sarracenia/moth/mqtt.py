7c1c3e4e510ddd95c8025db71bdb2e48639bb44e
Peter Silva <peter@bsqt.homeip.net>
2026-03-13 18:35:56 -0400

    remove *exchange* from mqtt for publishing.

--- sarracenia/moth/mqtt.py
+++ sarracenia/moth/mqtt.py
@@ -766,7 +766,7 @@ class MQTT(Moth):
                     del body[k]
             del body['_deleteOnPost']
 
-        if not exchange:
+        if not exchange and not self.o['mqttExchangeBeforeTopicPrefix']:
             if (type(self.o['exchange']) is list):
                 if (len(self.o['exchange']) > 1):
                     if 'post_exchangeSplit' in self.o:
@@ -796,7 +796,7 @@ class MQTT(Moth):
             if not 'posts' in message:
                 message['posts'] = []
 
-            message['posts'].append({ 'broker':str(self.o['broker']), 'topic': topic, 'exchange':exchange } )
+            message['posts'].append({ 'broker':str(self.o['broker']), 'topic': topic  } )
 
             del headers['topic']
 
--- sarracenia/postformat/__init__.py
+++ sarracenia/postformat/__init__.py
@@ -85,7 +85,7 @@ class PostFormat:
  
         p = options['publishers'][options['publisher_index']]
 
-        if p['broker'].url.scheme.startswith('mqtt'):
+        if p['broker'].url.scheme.startswith('mqtt') and p['mqttExchangeBeforeTopicPrefix']:
             if ( 'exchange' in p ) and ( 'topicPrefix' in p ):
                 if 'exchangeSplit' in p and p['exchangeSplit'] > 1:
                     idx = sum( bytearray(msg['identity']['value'], 'ascii')) % len(p['exchange'])
