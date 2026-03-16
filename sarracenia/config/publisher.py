b40f54b3864a10e3915a7e8881b75cd75e146301
Peter Silva <peter@bsqt.homeip.net>
2026-03-16 13:52:21 -0400

    publisher must include tlsRigour

--- sarracenia/config/publisher.py
+++ sarracenia/config/publisher.py
@@ -53,6 +53,9 @@ class Publisher(dict):
             logger.error("malformed publisher, missing (post_)exchange")
             return
 
+        if hasattr(options,'tlsRigour') :
+            self['tlsRigour'] = options.tlsRigour
+    
         if hasattr(options,'post_format') :
             self['format'] = options.post_format
         elif hasattr(options,'post_topicPrefix') and options.post_topicPrefix[0] in [ 'v02', 'v03' ]:
