04fbd94e280df58ff2e128e7e4d0e1625ad98a58
Reid Sunderland <reid.sunderland@ssc-spc.gc.ca>
2026-03-18 12:37:07 -0500

    that change should actually apply in all error cases

--- sarra/sr_amqp.py
+++ sarra/sr_amqp.py
@@ -582,8 +582,6 @@ class Queue:
                 except (BrokenPipeError, ConnectionResetError):
                     self.logger.error("lost connection to broker, attempting to reconnect")
                     self.hc.reconnect()
-                    bound = 0 # restart binding from the beginning
-                    break # out of for loop
                 except Exception as err:
                     self.logger.error("bind queue: %s to exchange: %s with key: %s failed with %s"
                                   % (self.name, exchange_name, exchange_key, err))
@@ -595,6 +593,8 @@ class Queue:
                 time.sleep(backoff)
                 if backoff < 60:
                     backoff *= 2
+                bound = 0 # restart binding from the beginning
+                break # out of for loop
 
         self.logger.debug("queue build done")
 
