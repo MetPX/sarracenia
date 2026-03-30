7e72cacf7f4b0d79eb987b1fe1c92826fc1b7036
Reid Sunderland <reid@yoreinc.net>
2026-03-30 09:53:17 -0500

    Revert "adjust fstrings in unbinding"
    This reverts commit da1fcc51abdc8f2ad5eec4a384ae2cb06c244422.

--- sarracenia/moth/amqp.py
+++ sarracenia/moth/amqp.py
@@ -432,9 +432,11 @@ class AMQP(Moth):
                     prefix= b['prefix'] if 'prefix' in b else None
                     topic = b['topic']
                     if self.o['dry_run']:
-                        logger.info( f"unbinding (dry run) {queue['name']} with {topic} from {exchange} (as: {broker_str}) "  )
+                        logger.info('unbinding (dry run) %s with %s from %s (as: %s)' % \
+                            ( queue['name'], topic, exchange, broker_str ) )
                     else:
-                        logger.info( f"unbinding {queue['name']} with {topic} from {exchange} (as: {broker_str})" )
+                        logger.info('unbinding %s with %s from %s (as: %s)' % \
+                            ( queue['name'], topic, exchange, broker_str ) )
                         if exchange:
                             self.management_channel.queue_unbind(queue['name'], exchange, topic)
 
