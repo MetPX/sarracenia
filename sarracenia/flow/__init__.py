abab3c67ec44e767b3cf49921def63fea63829bc
Reid Sunderland <reid.sunderland@ssc-spc.gc.ca>
2026-06-19 13:35:44 +0000

    put both counts in log msg

--- sarracenia/flow/__init__.py
+++ sarracenia/flow/__init__.py
@@ -641,8 +641,9 @@ class Flow:
                 if (self.o.retryEmptyBeforeExit and "retry" in self.metrics
                     and (self.metrics['retry']['msgs_in_post_retry'] > 0
                          or self.metrics['retry']['msgs_in_download_retry'] > 0) ):
-                    logger.info( f"retryEmptyBeforeExit=True and there are still "
-                        f"{self.metrics['retry']['msgs_in_post_retry']} messages in the retry queue(s).")
+                    logger.info("retryEmptyBeforeExit=True and there are still messages in the retry queues"
+                                + f"(post: {self.metrics['retry']['msgs_in_post_retry']}, "
+                                + f"download: {self.metrics['retry']['msgs_in_download_retry']})")
                     # Messages can't be retried before housekeeping has run, so run it right now
                     next_housekeeping = now - 1
                     # sleep for a bit (self.o.sleep is <0)
