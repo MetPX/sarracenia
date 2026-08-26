22429942dc24ab28b5cd796ca8d110b54847e531
Andre LeBlanc <andre.leblanc@ssc-spc.gc.ca>
2026-08-26 18:11:58 +0000

    Add mail fetch changes from testings

--- sarracenia/flowcb/download/mail_ingest.py
+++ sarracenia/flowcb/download/mail_ingest.py
@@ -2,28 +2,24 @@
 
 """
 
-This downloader corresponds to the poll.mail class.
-Fetches emails from a server using the specified protocol,
-either pop3 or imap. The imaplib/poplib implementations in
-Python use the most secure SSL settings by default: 
-PROTOCOL_TLS, OP_NO_SSLv2, and OP_NO_SSLv3.
-Compatible with Python 2.7+.
+Description
+    This downloader corresponds to the poll.mail class.
+    Fetches emails from a server using the specified protocol,
+    either pop3 or imap. The imaplib/poplib implementations in
+    Python use the most secure SSL settings by default:
+    PROTOCOL_TLS, OP_NO_SSLv2, and OP_NO_SSLv3.
 
-download_email_ingest: a sample do_download option for sr_subscribe.
-                        connects to an email server with the provided
-                        credentials and posts all new messages.
+Usage
 
-usage:
-        in an sr_subscribe configuration file:
-
-        destination [imap|imaps|pop|pops]://[user[:password]@]host[:port]/
-        do_download download_email_ingest
-
+    In ``credentials.conf``
+        [imap|imaps|pop|pops]://[user[:password]@]host[:port]/
         IMAP over SSL uses 993, POP3 over SSL uses 995
         IMAP unsecured uses 143, POP3 unsecured uses 110
-
         Full credentials must be in credentials.conf.
         If port is not specified it'll default to the ones above based on protocol/ssl setting.
+
+    In a configuration file
+        callback download.mail_ingest
 
 """
 
@@ -33,15 +29,13 @@ import sarracenia
 from sarracenia.flowcb import FlowCB
 import sarracenia.identity
 
-
 logger = logging.getLogger(__name__)
 
 class Mail_ingest(FlowCB):
 
         def download(self, msg) -> bool:
 
-                #ok, details = self.o.credentials.get(parent.msg.notice.split()[1])
-                ok, details = self.o.credentials.get(msg.baseUrl)
+                ok, details = self.o.credentials.get(msg['baseUrl'])
                 if ok: 
                         setting         = details.url
                         user            = setting.username
@@ -50,7 +44,7 @@ class Mail_ingest(FlowCB):
                         protocol        = setting.scheme.lower() 
                         port            = setting.port
                 else:
-                        logger.error(f"download_email_ingest: destination has invalid credentials: {msg['baseUrl']}")
+                        logger.error(f"destination has invalid credentials: {msg['baseUrl']}")
                         return False
 
                 if not port:
@@ -69,7 +63,7 @@ class Mail_ingest(FlowCB):
                                         mailman = imaplib.IMAP4_SSL(server, port=port)
                                         mailman.login(user, password)
                                 except imaplib.IMAP4.error as e:
-                                        logger.error(f"download_email_ingest imaplib connection error: {e}")
+                                        logger.error(f"imaplib connection error: {e}")
                                         return False
 
                         elif protocol == "imap":
@@ -77,7 +71,7 @@ class Mail_ingest(FlowCB):
                                         mailman = imaplib.IMAP4(server, port=port)
                                         mailman.login(user, password)
                                 except imaplib.IMAP4.error as e:
-                                        logger.error(f"download_email_ingest imaplib connection error: {e}")
+                                        logger.error(f"imaplib connection error: {e}")
                                         return False
                         else: return False
 
@@ -86,7 +80,7 @@ class Mail_ingest(FlowCB):
                         for index in data[0].split():
                                 r, d = mailman.fetch(index, '(RFC822)')
                                 msg = d[0][1].decode("utf-8", "ignore") + "\n"
-                                logger.info(f"download_email_ingest downloaded file: {msg['new_dir']}"+'/'+msg['new_file'])
+                                logger.info(f"downloaded file: {msg['new_dir']}"+'/'+msg['new_file'])
                                 with open(msg['new_dir']+'/'+msg['new_file'], 'w') as f:
                                        f.write(msg)
                                        f.close()
@@ -105,7 +99,7 @@ class Mail_ingest(FlowCB):
                                         mailman.user(user)
                                         mailman.pass_(password)
                                 except poplib.error_proto as e:
-                                        logger.error(f"download_email_ingest pop3 connection error: {e}")
+                                        logger.error(f"pop3 connection error: {e}")
                                         return False
 
                         elif protocol == "pop":
@@ -114,7 +108,7 @@ class Mail_ingest(FlowCB):
                                         mailman.user(user)
                                         mailman.pass_(password)
                                 except poplib.error_proto as e:
-                                        logger.error(f"download_email_ingest pop3 connection error: {e}")
+                                        logger.error(f"pop3 connection error: {e}")
                                         return False
                         else: return False
                         # only retrieves msgs that haven't triggered internal pop3 'read' flag
@@ -130,29 +124,30 @@ class Mail_ingest(FlowCB):
 
                                 # if it worked, write it locally, update message as needed.
                                 if msgid == msg['new_file']:                                        
-                                    logger.info(f"download_email_ingest downloaded file: {msg['new_dir']}"+'/'+msg['new_file'])
+                                    logger.info(f"downloaded file: {msg['new_dir']}"+'/'+msg['new_file'])
                                     
-                                    sumalgo = sarracenia.identity.Identity.factory(self.o.identity_method)
-                                    sumalgo.set_path(path)
+                                    sumalgo = sarracenia.identity.Identity.factory(method=self.o.identity_method)
+                                    sumalgo.set_path('')
                                     with open(msg['new_dir']+'/'+msg['new_file'], 'w') as f:
                                         sumalgo.update(email_message)
                                         f.write(email_message)
                                         f.close()
                                 
-                                    message['size'] = len(bytes(email_message,'utf8'))
-                                    message['identity'] = { 'method': self.o.identity_method, 'value': sumalgo.value }
+                                    msg['size'] = len(bytes(email_message,'utf8'))
+                                    msg['identity'] = { 'method': self.o.identity_method, 'value': sumalgo.value }
                                     if self.o.delete :
                                         mailman.dele(index+1)
                                     found=True
                                     break
 
+                        mailman.quit()
                         if not found:
                            logger.error('message not found on server, giving up')
+                           # If mail is not found on target server, we should append to the retry queue.
+                           return False
 
-                        mailman.quit()
-                        #v2 parent.msg.set_parts()-> sr3? msg.updatePaths( self.o, msg['new_dir'], msg['new_file'] )
                         return True
 
                 else:
-                        logger.error("download_email_ingest destination protocol must be one of 'imap/imaps' or 'pop/pops'.")
+                        logger.error("destination protocol must be one of 'imap/imaps' or 'pop/pops'.")
                         return False
