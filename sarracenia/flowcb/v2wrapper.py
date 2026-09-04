d5d750beee8aa10985f1311b8602bfe5517db1a7
Reid Sunderland <reid.sunderland@ssc-spc.gc.ca>
2026-09-04 10:35:09 -0500

    fix mistake

--- sarracenia/flowcb/v2wrapper.py
+++ sarracenia/flowcb/v2wrapper.py
@@ -78,7 +78,7 @@ def sumstrFromMessage( msg ) -> str:
                 sumstr = f'r,{hash.hexdigest()}'
             else:
                 sumstr = f'm,{hash.hexdigest()}'
-        elif 'rename' not in msg:
+        elif 'rename' not in msg['fileOp']:
             logger.error(f"unknown fileOp: {msg['fileOp']}" )
     return sumstr
 
